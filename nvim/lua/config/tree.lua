f5b0f17bade3332fc0b904bc0f73f23ade139bf0
Kevin Traver <kevintraver@kevintraver.com>
2021-11-12 14:06:43 -0700

    add tree keymaps

--- nvim/lua/config/tree.lua
+++ nvim/lua/config/tree.lua
@@ -1,1 +1,4 @@
 require'nvim-tree'.setup {}
+
+vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>NvimTreeToggle<cr>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>T", "<cmd>NvimTreeFindFile<cr>", {noremap = true, silent = true})
