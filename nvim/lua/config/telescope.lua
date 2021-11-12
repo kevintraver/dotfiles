7c6fd2d43b9038c24602a47d8dd174f0fbc221e7
Kevin Traver <kevintraver@kevintraver.com>
2021-11-12 12:35:35 -0700

    telescope show hidden files

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,3 +1,3 @@
-vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files({hidden=true})<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fg', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fr', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
