8afcb7be88c772a305e84a592b280860b27ee664
Kevin Traver <kevintraver@kevintraver.com>
2021-11-13 18:34:09 -0700

    add buffer close command

--- nvim/lua/config/barbar.lua
+++ nvim/lua/config/barbar.lua
@@ -1,7 +1,8 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map('n', '<C-[>', '<cmd>BufferPick<CR>', opts)
-
 map('n', '<C-p>', '<cmd>BufferPrevious<CR>', opts)
 map('n', '<C-n>', '<cmd>BufferNext<CR>', opts)
+
+map('n', '<M-w>', '<cmd>BufferClose<CR>', opts)
+map('n', '<M-t>', '<cmd>BufferPick<CR>', opts)
