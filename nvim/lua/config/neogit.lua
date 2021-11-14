4b9b8c9de4f774ee18341b5efd4d2510c09a28e1
Kevin Traver <kevintraver@kevintraver.com>
2021-11-13 18:23:47 -0700

    neogit set insert on commit

--- nvim/lua/config/neogit.lua
+++ nvim/lua/config/neogit.lua
@@ -3,6 +3,7 @@ vim.api.nvim_set_keymap('n', '<Leader>gg', '<cmd>Neogit<CR>', { silent = true })
 local neogit = require("neogit")
 
 neogit.setup {
+  disable_insert_on_commit = false,
   integrations = {
     diffview = true
   }
