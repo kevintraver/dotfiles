fc95fe692b157bc988456e5e0329012e2028814e
Kevin Traver <kevintraver@kevintraver.com>
2021-11-13 19:28:16 -0700

    add back BufferPick

--- nvim/lua/config/barbar.lua
+++ nvim/lua/config/barbar.lua
@@ -1,6 +1,8 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
+map('n', '<C-[>', '<cmd>BufferPick<CR>', opts)
+
 map('n', '<C-p>', '<cmd>BufferPrevious<CR>', opts)
 map('n', '<C-n>', '<cmd>BufferNext<CR>', opts)
 
