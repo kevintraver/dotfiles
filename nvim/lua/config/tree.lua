1aa55204b77f90a4e5cb9aa504a689a8af455ddc
Kevin Traver <kevintraver@kevintraver.com>
2021-11-13 22:47:50 -0700

    require nvim tree setup

--- nvim/lua/config/tree.lua
+++ nvim/lua/config/tree.lua
@@ -1,2 +1,3 @@
+require('nvim-tree').setup {}
 vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>NvimTreeToggle<cr>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<leader>T", "<cmd>NvimTreeFindFile<cr>", {noremap = true, silent = true})
