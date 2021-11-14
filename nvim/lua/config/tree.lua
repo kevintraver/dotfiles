eadac5bfcd669ad68b70f7d9070c7efca2da6789
Kevin Traver <kevintraver@kevintraver.com>
2021-11-13 23:02:43 -0700

    fix tree working directories

--- nvim/lua/config/tree.lua
+++ nvim/lua/config/tree.lua
@@ -1,4 +1,13 @@
-require('nvim-tree').setup {}
+vim.g.nvim_tree_respect_buf_cwd = 1
+
+require("nvim-tree").setup({
+  update_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_cwd = true
+  },
+})
+
 vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>NvimTreeToggle<cr>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<leader>T", "<cmd>NvimTreeFindFile<cr>", {noremap = true, silent = true})
 
