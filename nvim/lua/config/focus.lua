6a156153af848e253896e4ff541c8aab0cd24738
Kevin Traver <kevintraver@kevintraver.com>
2021-11-13 21:49:10 -0700

    remape focus split

--- nvim/lua/config/focus.lua
+++ nvim/lua/config/focus.lua
@@ -1,6 +1,4 @@
 require("focus").setup()
 
-vim.api.nvim_set_keymap('n', '<S-l>', '<cmd>FocusSplitNicely<CR>', { silent = true })
-
--- vim.api.nvim_set_keymap('n', '<c-l>', ":lua require('focus').split_nicely()<CR>", { silent = true })
-
+vim.api.nvim_set_keymap('n', '<M-d>', '<cmd>FocusSplitNicely<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<M-l>', '<cmd>FocusSplitNicely<CR>', { silent = true })
