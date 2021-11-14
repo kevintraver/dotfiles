e71751ff2cc774b4bf5d3ee177da9c6b137efec2
Kevin Traver <kevintraver@kevintraver.com>
2021-11-13 18:06:35 -0700

    remove tree settings

--- nvim/lua/config/tree.lua
+++ nvim/lua/config/tree.lua
@@ -1,12 +1,2 @@
 vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>NvimTreeToggle<cr>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<leader>T", "<cmd>NvimTreeFindFile<cr>", {noremap = true, silent = true})
-
-vim.g.nvim_tree_respect_buf_cwd = 1
-
-require("nvim-tree").setup({
-  update_cwd = true,
-  update_focused_file = {
-    enable = true,
-    update_cwd = true
-  },
-})
