1d30d242f908120ee64bc25510975b2d3bdafa13
Kevin Traver <kevintraver@kevintraver.com>
2021-11-14 15:35:18 -0700

    update telescope keymaps

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -6,7 +6,8 @@ vim.api.nvim_set_keymap('n', '<leader>ei', [[<cmd>edit ~/.config/nvim/init.lua<c
 vim.api.nvim_set_keymap('n', '<leader>es', [[<cmd>edit ~/.config/nvim/lua/settings.lua<cr>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>ep', [[<cmd>edit ~/.config/nvim/lua/plugins.lua<cr>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>ek', [[<cmd>edit ~/.config/nvim/lua/keymappings.lua<cr>]], { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>et', [[<cmd>edit ~/.config/nvim/lua/config/telescope.lua<cr>]], { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>ef', [[<cmd>edit ~/.config/nvim/lua/config/telescope.lua<cr>]], { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>et', [[<cmd>edit ~/.config/nvim/lua/terminal.lua<cr>]], { noremap = true })
 
 vim.api.nvim_set_keymap('n', '<leader>ez', [[<cmd>edit ~/.zshrc<cr>]], { noremap = true })
 
