4e81e111cb600b4af8fcebe3b7d2a66e9497b9ff
Kevin Traver <kevintraver@kevintraver.com>
2021-11-14 12:24:37 -0700

    add vim move keys, and remove extra focus key

--- nvim/lua/config/focus.lua
+++ nvim/lua/config/focus.lua
@@ -1,4 +1,3 @@
 require("focus").setup({tmux = true})
 
 vim.api.nvim_set_keymap('n', '<M-d>', '<cmd>FocusSplitNicely<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', '<M-l>', '<cmd>FocusSplitNicely<CR>', { silent = true })
