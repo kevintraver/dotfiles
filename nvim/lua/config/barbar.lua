29d5a9bf4a6d83c66614d3ea497b138e4083d86e
Kevin Traver <kevintraver@kevintraver.com>
2021-11-14 07:24:52 -0700

    update barbar keymaps

--- nvim/lua/config/barbar.lua
+++ nvim/lua/config/barbar.lua
@@ -1,9 +1,12 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map('n', '<C-[>', '<cmd>BufferPick<CR>', opts)
+map('n', '<Space><Space>', '<cmd>BufferPick<CR>', opts)
 
 map('n', '<C-p>', '<cmd>BufferPrevious<CR>', opts)
 map('n', '<C-n>', '<cmd>BufferNext<CR>', opts)
+
+map('n', '<Tab>', '<cmd>BufferNext<CR>', opts)
+map('n', '<S-Tab>', '<cmd>BufferPrevious<CR>', opts)
 
 map('n', '<M-t>', '<cmd>BufferPick<CR>', opts)
