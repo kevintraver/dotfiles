02fd55690654685cb3ac4037d95355cd40217041
Kevin Traver <kevintraver@kevintraver.com>
2021-11-14 06:44:54 -0700

    focus use tmux

--- nvim/lua/config/focus.lua
+++ nvim/lua/config/focus.lua
@@ -1,4 +1,4 @@
-require("focus").setup()
+require("focus").setup({tmux = true})
 
 vim.api.nvim_set_keymap('n', '<M-d>', '<cmd>FocusSplitNicely<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<M-l>', '<cmd>FocusSplitNicely<CR>', { silent = true })
