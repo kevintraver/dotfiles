1133a84246c7268a74d733844a20e3dbd7f74c98
Kevin Traver <kevintraver@kevintraver.com>
2021-11-14 21:54:08 -0700

    use autoinsert - works better for nvr

--- nvim/lua/terminal.lua
+++ nvim/lua/terminal.lua
@@ -10,14 +10,11 @@ map('t', '<Esc>', [[<c-\><c-n>]], { silent = true })
 vim.cmd [[
     autocmd TermOpen * setlocal listchars= nonumber norelativenumber nocursorline
     autocmd TermOpen * :SendHere
-    autocmd TermOpen term://* startinsert
-    autocmd BufWinEnter,BufEnter,BufNew,BufLeave term://* stopinsert
 ]]
 
 vim.g.send_disable_mapping = true
 vim.g.neoterm_autoscroll = 1
 vim.g.neoterm_autoinsert = 0
-vim.g.neomux_no_term_autoinsert = 1
 
 map('n', '<M-Enter>', [[<Plug>SendLine]], { silent = true })
 map('v', '<M-Enter>', [[<Plug>Send]], { silent = true })
