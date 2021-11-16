deeaac41c8682b9bfa929b7db9da7988f99af377
Kevin Traver <kevintraver@kevintraver.com>
2021-11-15 19:35:24 -0700

    add quickfix to excluded_buftypes for focus

--- nvim/lua/config/focus.lua
+++ nvim/lua/config/focus.lua
@@ -1,3 +1,6 @@
-require("focus").setup({tmux = true})
+require("focus").setup({
+  tmux = true,
+  excluded_buftypes = { 'nofile', 'prompt', 'popup', 'quickfix'}
+})
 
 vim.api.nvim_set_keymap('n', '<M-l>', '<cmd>FocusSplitNicely<CR>', { silent = true })
