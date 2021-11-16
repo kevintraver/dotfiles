355d151fd86afd164e3aac7ae26598c5f840f5ab
Kevin Traver <kevintraver@kevintraver.com>
2021-11-15 22:33:57 -0700

    remove old bufferpick keymap

--- nvim/lua/config/barbar.lua
+++ nvim/lua/config/barbar.lua
@@ -2,6 +2,3 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 map('n', '<Space><Space>', '<cmd>BufferPick<CR>', opts)
-
-
-map('n', '<M-t>', '<cmd>BufferPick<CR>', opts)
