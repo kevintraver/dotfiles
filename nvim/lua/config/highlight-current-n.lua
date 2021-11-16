9b0736fdeaa087e165c58d36d23034995140e852
Kevin Traver <kevintraver@kevintraver.com>
2021-11-15 22:46:33 -0700

    set nohlsearch

--- nvim/lua/config/highlight-current-n.lua
+++ nvim/lua/config/highlight-current-n.lua
@@ -1,2 +1,7 @@
+require("highlight_current_n").setup{}
+
 vim.api.nvim_set_keymap('n', 'n', '<Plug>(highlight-current-n-n)', {noremap = false})
 vim.api.nvim_set_keymap('n', 'N', '<Plug>(highlight-current-n-N)', {noremap = false})
+
+vim.cmd "autocmd CmdlineEnter /,? :set hlsearch"
+vim.cmd "autocmd CmdlineLeave /,? :set nohlsearch"
