d337b81983eb2bb28cc0255c5fd9f887ad6d7e8a
Kevin Traver <kevintraver@kevintraver.com>
2021-11-18 09:23:36 -0700

    add keymap for git diff history

--- nvim/lua/config/diffview.lua
+++ nvim/lua/config/diffview.lua
@@ -1,1 +1,2 @@
 vim.api.nvim_set_keymap('n', '<leader>di', [[<cmd>DiffviewOpen<CR>]], { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>dh', [[<cmd>DiffviewFileHistory .<CR>]], { noremap = true })
