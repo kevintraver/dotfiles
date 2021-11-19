cc9b2e70e138e3cf0638257e1c0ba311941f4971
Kevin Traver <kevintraver@kevintraver.com>
2021-11-19 16:08:35 -0700

    change diffview keymap (conflicts with hunk stage)

--- nvim/lua/config/diffview.lua
+++ nvim/lua/config/diffview.lua
@@ -1,3 +1,3 @@
 vim.api.nvim_set_keymap('n', '<leader>df', [[<cmd>DiffviewOpen<CR>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>dv', [[<cmd>DiffviewOpen<CR>]], { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>dh', [[<cmd>DiffviewFileHistory .<CR>]], { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>dl', [[<cmd>DiffviewFileHistory .<CR>]], { noremap = true })
