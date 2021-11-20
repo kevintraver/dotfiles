5d6fd64bcd9ed282e5c24541c27893b7161026d0
Kevin Traver <kevintraver@kevintraver.com>
2021-11-19 19:37:02 -0700

    add additional keymap for telescope git status

--- nvim/lua/config/diffview.lua
+++ nvim/lua/config/diffview.lua
@@ -1,3 +1,4 @@
 vim.api.nvim_set_keymap('n', '<leader>df', [[<cmd>DiffviewOpen<CR>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>dv', [[<cmd>DiffviewOpen<CR>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>dl', [[<cmd>DiffviewFileHistory .<CR>]], { noremap = true })
+vim.api.nvim_set_keymap('n', '<M-G>', [[<cmd>lua require('telescope.builtin').git_status()<CR>]], { noremap = true, silent = true })
