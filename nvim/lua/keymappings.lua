f4474a855df980b73c6496669af8adef1b32f0dc
Kevin Traver <kevintraver@kevintraver.com>
2021-11-20 18:39:36 -0700

    add m-q keybinding in all modes

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -19,6 +19,10 @@ vim.api.nvim_set_keymap('n', '<M-s>', [[<cmd>w<CR>]], { noremap = true } )
 
 vim.api.nvim_set_keymap('n', '<M-q>', [[<Esc><Esc><cmd>qall<cr>]], { noremap = true } )
 vim.api.nvim_set_keymap('n', '<M-Q>', [[<Esc><Esc><cmd>qall!<cr>]], { noremap = true } )
+vim.api.nvim_set_keymap('i', '<M-q>', [[<Esc><Esc><cmd>qall<cr>]], { noremap = true } )
+vim.api.nvim_set_keymap('i', '<M-Q>', [[<Esc><Esc><cmd>qall!<cr>]], { noremap = true } )
+vim.api.nvim_set_keymap('v', '<M-q>', [[<Esc><Esc><cmd>qall<cr>]], { noremap = true } )
+vim.api.nvim_set_keymap('v', '<M-Q>', [[<Esc><Esc><cmd>qall!<cr>]], { noremap = true } )
 
 vim.api.nvim_set_keymap("n", ";", ":", { noremap = true })
 vim.api.nvim_set_keymap("v", ";", ":", { noremap = true })
