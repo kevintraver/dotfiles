ded460586aa66bbdfe291f9595076794b6ad02a1
Kevin Traver <kevintraver@kevintraver.com>
2021-11-23 06:57:30 -0800

    lsp setup part of navigator plugin

--- nvim/lua/config/lspconfig.lua
+++
@@ -1,5 +0,0 @@
-local lspconfig = require('lspconfig')
-local coq = require('coq')
-
-lspconfig.solargraph.setup(coq.lsp_ensure_capabilities{})
-lspconfig.tsserver.setup(coq.lsp_ensure_capabilities{})
