a4cf0602e90c536049dc375e140e16a21b637a05
Kevin Traver <kevintraver@kevintraver.com>
2021-11-23 13:12:09 -0800

    add h/l tree keymaps

--- nvim/lua/config/tree.lua
+++ nvim/lua/config/tree.lua
@@ -1,11 +1,21 @@
 vim.g.nvim_tree_respect_buf_cwd = 1
 
+local tree_cb = require "nvim-tree.config".nvim_tree_callback
+
 require("nvim-tree").setup({
+  view = {
+    mappings = {
+      list = {
+        { key = "l", cb = tree_cb("edit") },
+        { key = "h", cb = tree_cb("close_node") },
+      }
+    }
+  },
   update_cwd = true,
   update_focused_file = {
     enable = true,
     update_cwd = true
-  },
+  }
 })
 
 vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>NvimTreeToggle<cr>", {noremap = true, silent = true})
