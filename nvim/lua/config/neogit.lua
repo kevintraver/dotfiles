1b2e2e5129fe37e97b3a7ffde707d99e82f85104
Kevin Traver <kevintraver@kevintraver.com>
2021-11-23 06:06:10 -0800

    add git commit keymap

--- nvim/lua/config/neogit.lua
+++ nvim/lua/config/neogit.lua
@@ -1,6 +1,7 @@
 local opts = {noremap = true, silent = true}
 
 vim.api.nvim_set_keymap('n', '<Leader>gg', '<cmd>lua require("neogit").open()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>gc', '<cmd>lua require("neogit").open({ "commit" })<CR>', opts)
 vim.api.nvim_set_keymap('n', '<M-g>', '<cmd>lua require("neogit").open()<CR>', opts)
 
 vim.cmd("autocmd FileType NeogitCommitMessage nnoremap <buffer> <M-s> :wq<CR>")
