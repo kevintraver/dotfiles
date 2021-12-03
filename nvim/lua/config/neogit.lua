6553c37a3fef6d3474492d71050f9d88af062664
Kevin Traver <kevintraver@kevintraver.com>
2021-12-03 03:01:03 -0700

    add tmux movement keybindings for NeogitCommitMessage

--- nvim/lua/config/neogit.lua
+++ nvim/lua/config/neogit.lua
@@ -4,8 +4,12 @@ vim.api.nvim_set_keymap('n', '<Leader>gg', '<cmd>lua require("neogit").open()<CR
 vim.api.nvim_set_keymap('n', '<Leader>gc', '<cmd>lua require("neogit").open({ "commit" })<CR>', opts)
 vim.api.nvim_set_keymap('n', '<M-g>', '<cmd>lua require("neogit").open()<CR>', opts)
 
-vim.cmd("autocmd FileType NeogitCommitMessage nnoremap <buffer> <M-s> :wq<CR>")
-vim.cmd("autocmd FileType NeogitCommitMessage inoremap <buffer> <M-s> <C-\\><C-n>:wq<CR>")
+vim.cmd[[
+  autocmd FileType NeogitCommitMessage inoremap <silent><buffer> <c-k> <c-\><c-n><cmd>lua require("tmux").move_top()<cr>
+  autocmd FileType NeogitCommitMessage inoremap <silent><buffer> <m-k> <c-\><c-n><cmd>lua require("tmux").move_top()<cr>
+  autocmd FileType NeogitCommitMessage nnoremap <buffer> <M-s> :wq<CR>
+  autocmd FileType NeogitCommitMessage inoremap <buffer> <M-s> <c-\><c-n>:wq<CR>
+]]
 
 local neogit = require("neogit")
 
