d5f2a229d415ec7ae64bee98392e2f1ffb12d8e0
Kevin Traver <kevintraver@kevintraver.com>
2021-12-04 12:51:07 -0700

    add more telescope insert mode keymaps

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -7,6 +7,14 @@ local actions = require "telescope.actions"
 
 require('telescope').setup{
   defaults = {
+    mappings = {
+      i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-l>"] = require('telescope.actions').cycle_history_next,
+        ["<C-h>"] = require('telescope.actions').cycle_history_prev,
+      },
+    },
     file_ignore_patterns = {"node_modules", ".git/.*" }
   },
   extensions = {
