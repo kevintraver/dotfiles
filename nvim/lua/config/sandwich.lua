c91aeb865abd2aefea498736b503225b30239538
Kevin Traver <kevintraver@kevintraver.com>
2021-12-07 06:24:09 -0700

    update sandwich keymappings to use '

--- nvim/lua/config/sandwich.lua
+++ nvim/lua/config/sandwich.lua
@@ -2,15 +2,15 @@ vim.api.nvim_exec(
   [[
 
     let g:sandwich_no_default_key_mappings = 1
-    silent! nmap <unique><silent> gsd <Plug>(operator-sandwich-delete)<Plug>(operator-sandwich-release-count)<Plug>(textobj-sandwich-query-a)
-    silent! nmap <unique><silent> gsr <Plug>(operator-sandwich-replace)<Plug>(operator-sandwich-release-count)<Plug>(textobj-sandwich-query-a)
-    silent! nmap <unique><silent> gsdb <Plug>(operator-sandwich-delete)<Plug>(operator-sandwich-release-count)<Plug>(textobj-sandwich-auto-a)
-    silent! nmap <unique><silent> gsrb <Plug>(operator-sandwich-replace)<Plug>(operator-sandwich-release-count)<Plug>(textobj-sandwich-auto-a)
+    silent! nmap <unique><silent> 'd <Plug>(operator-sandwich-delete)<Plug>(operator-sandwich-release-count)<Plug>(textobj-sandwich-query-a)
+    silent! nmap <unique><silent> 'r <Plug>(operator-sandwich-replace)<Plug>(operator-sandwich-release-count)<Plug>(textobj-sandwich-query-a)
+    silent! nmap <unique><silent> 'db <Plug>(operator-sandwich-delete)<Plug>(operator-sandwich-release-count)<Plug>(textobj-sandwich-auto-a)
+    silent! nmap <unique><silent> 'rb <Plug>(operator-sandwich-replace)<Plug>(operator-sandwich-release-count)<Plug>(textobj-sandwich-auto-a)
 
     let g:operator_sandwich_no_default_key_mappings = 1
-    silent! map <unique> gsa <Plug>(operator-sandwich-add)
-    silent! xmap <unique> gsd <Plug>(operator-sandwich-delete)
-    silent! xmap <unique> gsr <Plug>(operator-sandwich-replace)
+    silent! map <unique> 'a <Plug>(operator-sandwich-add)
+    silent! xmap <unique> 'd <Plug>(operator-sandwich-delete)
+    silent! xmap <unique> 'r <Plug>(operator-sandwich-replace)
 
   ]],
 false)
