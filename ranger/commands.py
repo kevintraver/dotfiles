9b94d887ccd10bf2d0c86ef0f1972ca1d060bb5b
Kevin Traver <kevintraver@kevintraver.com>
2021-12-09 20:19:14 -0700

    add ranger sk integration

---
+++ ranger/commands.py
@@ -0,0 +1,38 @@
+# This is a sample commands.py.  You can add your own commands here.
+#
+# Please refer to commands_full.py for all the default commands and a complete
+# documentation.  Do NOT add them all here, or you may end up with defunct
+# commands when upgrading ranger.
+
+# A simple command for demonstration purposes follows.
+# -----------------------------------------------------------------------------
+
+from __future__ import (absolute_import, division, print_function)
+
+# You can import any python module as needed.
+import os
+
+# You always need to import ranger.api.commands here to get the Command class:
+from ranger.api.commands import Command
+
+
+# Any class that is a subclass of "Command" will be integrated into ranger as a
+# command.  Try typing ":my_edit<ENTER>" in ranger!
+
+from collections import deque
+
+
+
+class sk_select(Command):
+    def execute(self):
+        import subprocess
+        from ranger.ext.get_executables import get_executables
+        
+        sk = self.fm.execute_command('sk ',universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, _ = sk.communicate()
+        if sk.returncode == 0:
+            selected = os.path.abspath(stdout.strip())
+            if os.path.isdir(selected):
+                self.fm.cd(selected)
+            else:
+                self.fm.select_file(selected)
