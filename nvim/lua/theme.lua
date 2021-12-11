ef968143b19f8dbe59d6e738df56f83b819c31d6
Kevin Traver <kevintraver@kevintraver.com>
2021-12-10 23:52:15 -0700

    set CursorLine background color

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -9,3 +9,5 @@ vim.cmd[[colorscheme dracula]]
 vim.g.dracula_show_end_of_buffer = true
 vim.g.dracula_transparent_bg = true
 vim.g.dracula_lualine_bg_color = "#44475a"
+
+vim.api.nvim_command("highlight CursorLine guibg=#2B2E3B")
