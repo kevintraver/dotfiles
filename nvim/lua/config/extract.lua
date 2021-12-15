9be3992fc332cab1f9a8d27488c7f0c15806ccff
Kevin Traver <kevintraver@kevintraver.com>
2021-12-15 06:19:55 -0700

    update to lua style keymappings

--- nvim/lua/config/extract.lua
+++ nvim/lua/config/extract.lua
@@ -2,31 +2,14 @@ vim.g.extract_useDefaultMappings = false
 vim.g.extract_maxCount = 25
 vim.g.extract_autoCheckSystemClipboard = false
 
--- vim.api.nvim_set_keymap("n", "p", '[[<Plug>(extract-put)]]', { noremap = true })
--- vim.api.nvim_set_keymap("n", "P", '[[<Plug>(extract-Put)]]', { noremap = true })
---
--- vim.api.nvim_set_keymap("v", "p", '[[<Plug>(extract-put)]]', { noremap = true })
--- vim.api.nvim_set_keymap("v", "P", '[[<Plug>(extract-Put)]]', { noremap = true })
---
--- vim.api.nvim_set_keymap("v", "<leader>p", '[[<cmd>ExtractPin]]', {})
--- vim.api.nvim_set_keymap("v", "<leader>P", '[[<cmd>ExtractUnPin]]', {})
---
--- vim.api.nvim_set_keymap("n", "<c-n>", '[[<Plug>(extract-sycle)]]', { noremap = true })
--- vim.api.nvim_set_keymap("n", "<c-p>", '[[<Plug>(extract-Sycle)]]', { noremap = true })
+vim.api.nvim_set_keymap("n", "p", [[<Plug>(extract-put)]], {})
+vim.api.nvim_set_keymap("n", "P", [[<Plug>(extract-Put)]], {})
 
+vim.api.nvim_set_keymap("v", "p", [[<Plug>(extract-put)]], {})
+vim.api.nvim_set_keymap("v", "P", [[<Plug>(extract-Put)]], {})
 
-vim.cmd[[
+vim.api.nvim_set_keymap("v", "<leader>p", [[<cmd>ExtractPin]], {})
+vim.api.nvim_set_keymap("v", "<leader>P", [[<cmd>ExtractUnPin]], {})
 
-  nmap p <Plug>(extract-put)
-  nmap P <Plug>(extract-Put)
-
-  vmap p <Plug>(extract-put)
-  vmap P <Plug>(extract-Put)
-
-  nmap <leader>p :ExtractPin<cr>
-  nmap <leader>P :ExtractUnPin<cr>
-
-  map <c-n> <Plug>(extract-sycle)
-  map <c-p> <Plug>(extract-Sycle)
-
-]]
+vim.api.nvim_set_keymap("", "<c-n>", [[<Plug>(extract-sycle)]], {})
+vim.api.nvim_set_keymap("", "<c-p>", [[<Plug>(extract-Sycle)]], {})
