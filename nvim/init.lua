0bcca97352a3948eb1aa87c7e713f8d728817b12
Kevin Traver <kevintraver@kevintraver.com>
2022-06-20 20:20:24 -0600

    add vim-subversive

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,9 @@
 require 'plugins'
 
 vim.cmd('runtime macros/sandwich/keymap/surround.vim')
+
+vim.api.nvim_set_keymap('n', 'r', [[<Plug>(SubversiveSubstitute)]], {})
+vim.api.nvim_set_keymap('v', 'r', [[<Plug>(SubversiveSubstitute)]], {})
+
+vim.api.nvim_set_keymap('n', 'rl', [[<Plug>(SubversiveSubstituteLine)]], {})
+vim.api.nvim_set_keymap('v', 'rl', [[<Plug>(SubversiveSubstituteLine)]], {})
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,5 +3,6 @@ return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use 'ggandor/lightspeed.nvim'
   use 'machakann/vim-sandwich'
+  use 'svermeulen/vim-subversive'
 
 end)
