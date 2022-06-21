b1333fc87429d3f3e33bfa6644f6d03fac6a3c92
Kevin Traver <kevintraver@kevintraver.com>
2022-06-20 20:35:54 -0600

    Add vim-exchange

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,8 +1,11 @@
 return require('packer').startup(function()
 
   use 'wbthomason/packer.nvim'
+
   use 'ggandor/lightspeed.nvim'
+
   use 'machakann/vim-sandwich'
   use 'svermeulen/vim-subversive'
+  use 'tommcdo/vim-exchange'
 
 end)
