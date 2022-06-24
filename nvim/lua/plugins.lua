3dfc47f6ea4de68c05ee263d4449cbcd478838bc
Kevin Traver <kevintraver@kevintraver.com>
2022-06-23 18:32:40 -0600

    Update comments

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,10 +1,10 @@
 return require("packer").startup(function()
 
-  -- A use-package inspired plugin manager for Neovim
+  -- A use-package inspired plugin manager for Neovim.
   -- https://github.com/wbthomason/packer.nvim
   use("wbthomason/packer.nvim")
 
-  -- Lightspeed is a motion plugin for Neovim
+  -- Lightspeed is a motion plugin for Neovim.
   -- https://github.com/ggandor/lightspeed.nvim
   use("ggandor/lightspeed.nvim")
 
@@ -20,15 +20,15 @@ return require("packer").startup(function()
   -- https://github.com/gbprod/substitute.nvim
   use("gbprod/substitute.nvim")
 
-  -- Improved Yank and Put functionalities for Neovim
+  -- Improved Yank and Put functionalities for Neovim.
   -- https://github.com/gbprod/yanky.nvim
   use("gbprod/yanky.nvim")
 
-  -- Plugin that adds a cut operation separate from delete
+  -- Plugin that adds a cut operation separate from delete.
   -- https://github.com/gbprod/cutlass.nvim
   use("gbprod/cutlass.nvim")
 
-  -- Vim plugin for copying to the system clipboard with text-objects and motions
+  -- Vim plugin for copying to the system clipboard with text-objects and motions.
   -- https://github.com/christoomey/vim-system-copy
   use("christoomey/vim-system-copy")
 
