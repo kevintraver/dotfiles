109b3e80c0ed7df421fd833703331f1514cb729b
Kevin Traver <kevintraver@kevintraver.com>
2023-08-05 13:13:10 -0600

    Revert "neovim: add nvim-substitute keymaps for replace line"
    This reverts commit e6312185993247ec2892cff3f220f972dba08458.

--- nvim/init.lua
+++ nvim/init.lua
@@ -44,12 +44,8 @@ require("substitute").setup({
 
 vim.keymap.set("n", "r", [[<cmd>lua require('substitute').operator()<cr>]], { noremap = true })
 vim.keymap.set("x", "r", [[<cmd>lua require('substitute').visual()<cr>]], { noremap = true })
-vim.keymap.set("n", "rl", [[<cmd>lua require('substitute').line()<cr>]], { noremap = true })
-vim.keymap.set("n", "rL", [[<cmd>lua require('substitute').eol()<cr>]], { noremap = true })
 vim.keymap.set("n", "R", [["*<cmd>lua require('substitute').operator()<cr>]], { noremap = true })
 vim.keymap.set("x", "R", [["*<cmd>lua require('substitute').visual()<cr>]], { noremap = true })
-vim.keymap.set("n", "Rl", [["*<cmd>lua require('substitute').line()<cr>]], { noremap = true })
-vim.keymap.set("n", "RL", [["*<cmd>lua require('substitute').eol()<cr>]], { noremap = true })
 
 vim.keymap.set("n", "cx", "<cmd>lua require('substitute.exchange').operator()<cr>", { noremap = true })
 vim.keymap.set("n", "cxx", "<cmd>lua require('substitute.exchange').line()<cr>", { noremap = true })
