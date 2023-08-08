327989ea95543419ed38884a0f86d9c12e2d4e95
Kevin Traver <kevintraver@kevintraver.com>
2023-08-08 13:04:15 -0600

    update spider w keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,15 +43,21 @@ require("Comment").setup()
 -- gomove setup
 require("gomove").setup({ map_defaults = true })
 
-vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+vim.keymap.set({ "n", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+vim.keymap.set(
+  { "o" },
+  "w",
+  "<cmd>lua require('spider').motion('w', { skipInsignificantPunctuation = false } )<CR>",
+  { desc = "Spider-w" }
+)
+
 vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
 vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
 vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-ge" })
 
 -- substitute setup and keymaps
 require("substitute").setup({
-  exchange = {
-    use_esc_to_cancel = false,
+  exchange = {    use_esc_to_cancel = false,
   },
 })
 
