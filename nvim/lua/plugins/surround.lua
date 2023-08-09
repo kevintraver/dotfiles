2da26a184f27992e396a130ff7d50627e3f836ae
Kevin Traver <kevintraver@kevintraver.com>
2023-08-09 15:58:31 -0600

    use default surround keymaps
    Surround keymaps don’t conflict with leap

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -2,12 +2,6 @@ return {
   "kylechui/nvim-surround",
   config = function()
     require("nvim-surround").setup({
-      keymaps = {
-        normal = "'",
-        visual = "'",
-        delete = "d'",
-        change = "c'",
-      },
       move_cursor = false,
     })
   end,
