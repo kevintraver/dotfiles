777295e88444a1311c65deeea067ac8a29ec1bc0
Kevin Traver <kevintraver@kevintraver.com>
2023-08-09 17:33:30 -0600

    leap-spooky, dont use remote window

--- nvim/lua/plugins/leap-spooky.lua
+++ nvim/lua/plugins/leap-spooky.lua
@@ -1,6 +1,13 @@
 return {
   "ggandor/leap-spooky.nvim",
   config = function()
-    require("leap-spooky").setup()
+    require("leap-spooky").setup({
+      affixes = {
+        -- The cursor moves to the targeted object, and stays there.
+        magnetic = { window = "m" },
+        -- The operation is executed seemingly remotely (the cursor boomerangs back afterwards).
+        remote = { window = "r" },
+      },
+    })
   end,
 }
