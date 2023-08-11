5c1263991d5230a709734e5f62360f3a64689c96
Kevin Traver <kevintraver@kevintraver.com>
2023-08-11 10:30:12 -0600

    update restOfIndentation and restOfParagraph keymaps

--- nvim/lua/plugins/various-textobjs.lua
+++ nvim/lua/plugins/various-textobjs.lua
@@ -1,4 +1,13 @@
 return {
   "chrisgrieser/nvim-various-textobjs",
-  opts = { useDefaultKeymaps = true },
+  opts = {
+    disabledKeymaps = {
+      "r",
+      "R",
+    },
+    useDefaultKeymaps = true,
+  },
+
+  vim.keymap.set({ "o", "x" }, "ri", "<cmd>lua require('various-textobjs').restOfIndentation()<CR>"),
+  vim.keymap.set({ "o", "x" }, "rp", "<cmd>lua require('various-textobjs').restOfParagraph()<CR>"),
 }
