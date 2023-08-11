658d3fbd29b1099870adebec316fe5a954a1009f
Kevin Traver <kevintraver@kevintraver.com>
2023-08-10 21:16:26 -0600

    Set change surround keymap to rs (mnemonic replace surround)
    Add additional surround keymaps to avoid conflicting with leap

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -3,6 +3,12 @@ return {
   config = function()
     require("nvim-surround").setup({
       move_cursor = false,
+      keymaps = {
+        visual = "ys",
+        visual_line = "yS",
+        change = "rs",
+        change_line = "rS",
+      },
     })
   end,
 }
