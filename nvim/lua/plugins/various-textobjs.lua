3421068e40f08c21867629067bd09a6001eb142d
Kevin Traver <kevintraver@kevintraver.com>
2023-08-11 12:55:22 -0600

    use custom keymaps for various-textobjects
    dont map defaults

--- nvim/lua/plugins/various-textobjs.lua
+++ nvim/lua/plugins/various-textobjs.lua
@@ -1,13 +1,34 @@
 return {
   "chrisgrieser/nvim-various-textobjs",
   opts = {
-    disabledKeymaps = {
-      "r",
-      "R",
-    },
-    useDefaultKeymaps = true,
+    useDefaultKeymaps = false,
   },
 
+  vim.keymap.set({ "o", "x" }, "iS", "<cmd>lua require('various-textobjs').subword(true)<CR>"),
+  vim.keymap.set({ "o", "x" }, "aS", "<cmd>lua require('various-textobjs').subword(false)<CR>"),
+
+  vim.keymap.set({ "o", "x" }, "ii", "<cmd>lua require('various-textobjs').indentation(true, true)<CR>"),
+  vim.keymap.set({ "o", "x" }, "ai", "<cmd>lua require('various-textobjs').indentation(false, true)<CR>"),
+  vim.keymap.set({ "o", "x" }, "iI", "<cmd>lua require('various-textobjs').indentation(true, true)<CR>"),
+  vim.keymap.set({ "o", "x" }, "aI", "<cmd>lua require('various-textobjs').indentation(false, false)<CR>"),
+
   vim.keymap.set({ "o", "x" }, "ri", "<cmd>lua require('various-textobjs').restOfIndentation()<CR>"),
+
   vim.keymap.set({ "o", "x" }, "rp", "<cmd>lua require('various-textobjs').restOfParagraph()<CR>"),
+
+  vim.keymap.set({ "n", "o", "x" }, "L", "$"),
+  vim.keymap.set({ "n", "o", "x" }, "H", "^"),
+
+  vim.keymap.set({ "o", "x" }, "il", "<cmd>lua require('various-textobjs').lineCharacterwise(true)<CR>"),
+  vim.keymap.set({ "o", "x" }, "al", "<cmd>lua require('various-textobjs').lineCharacterwise(false)<CR>"),
+
+  vim.keymap.set({ "o", "x" }, "ik", "<cmd>lua require('various-textobjs').key(true)<CR>"),
+  vim.keymap.set({ "o", "x" }, "ak", "<cmd>lua require('various-textobjs').key(false)<CR>"),
+
+  vim.keymap.set({ "o", "x" }, "iv", "<cmd>lua require('various-textobjs').value(true)<CR>"),
+  vim.keymap.set({ "o", "x" }, "av", "<cmd>lua require('various-textobjs').value(false)<CR>"),
+
+  vim.keymap.set({ "o", "x" }, "%", "<cmd>lua require('various-textobjs').toNextClosingBracket()<CR>"),
+
+  vim.keymap.set({ "o", "x" }, "gG", "<cmd>lua require('various-textobjs').entireBuffer()<CR>"),
 }
