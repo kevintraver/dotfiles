9eda8e9ac219ff7458376ca2f39447cbabea7dc3
Kevin Traver <kevintraver@kevintraver.com>
2023-08-11 14:44:06 -0600

    set ns-textobject disable_builtin_mapping and chars

--- nvim/lua/plugins/ns-textobject.lua
+++ nvim/lua/plugins/ns-textobject.lua
@@ -2,6 +2,12 @@ return {
   "XXiaoA/ns-textobject.nvim",
   dependencies = { "nvim-surround" },
   config = function()
-    require("ns-textobject").setup()
+    require("ns-textobject").setup({
+      disable_builtin_mapping = {
+        enabled = true,
+        -- list of char which shouldn't mapping by auto_mapping
+        chars = { "a", "i", "b", "B", "t", "`", "'", '"', "{", "}", "(", ")", "[", "]", "<", ">" },
+      },
+    })
   end,
 }
