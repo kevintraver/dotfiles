0fbc26d18b21f08810186de31169e671b2c3f1ab
Kevin Traver <kevintraver@kevintraver.com>
2023-08-15 11:27:35 -0600

    add dial

---
+++ nvim/lua/plugins/dial.lua
@@ -0,0 +1,13 @@
+return {
+  "monaqa/dial.nvim",
+  config = function()
+    vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal(), {})
+    vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal(), {})
+    vim.keymap.set("n", "g<C-a>", require("dial.map").inc_gnormal(), {})
+    vim.keymap.set("n", "g<C-x>", require("dial.map").dec_gnormal(), {})
+    vim.keymap.set("v", "<C-a>", require("dial.map").inc_visual(), {})
+    vim.keymap.set("v", "<C-x>", require("dial.map").dec_visual(), {})
+    vim.keymap.set("v", "g<C-a>", require("dial.map").inc_gvisual(), {})
+    vim.keymap.set("v", "g<C-x>", require("dial.map").dec_gvisual(), {})
+  end,
+}
