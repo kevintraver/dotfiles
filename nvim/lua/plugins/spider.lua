610f51f7a000255eb31b5e9b4a417a0e9283fd59
Kevin Traver <kevintraver@kevintraver.com>
2023-08-15 10:16:39 -0600

    spider: remove w operator mode keymap

--- nvim/lua/plugins/spider.lua
+++ nvim/lua/plugins/spider.lua
@@ -2,12 +2,6 @@ return {
   "chrisgrieser/nvim-spider",
   config = function()
     vim.keymap.set({ "n", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
-    vim.keymap.set(
-      { "o" },
-      "w",
-      "<cmd>lua require('spider').motion('w', { skipInsignificantPunctuation = false } )<CR>",
-      { desc = "Spider-w" }
-    )
 
     vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
     vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
