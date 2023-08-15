53e02fb4ae81acd0314d78f5b9ba2e27196c03e6
Kevin Traver <kevintraver@kevintraver.com>
2023-08-15 05:18:33 -0600

    substitute: remove noremap in keymaps

--- nvim/lua/plugins/substitute.lua
+++ nvim/lua/plugins/substitute.lua
@@ -6,14 +6,14 @@ return {
       exchange = { use_esc_to_cancel = false },
     })
 
-    vim.keymap.set("n", "r", [[<cmd>lua require('substitute').operator()<cr>]], { noremap = true })
-    vim.keymap.set("x", "r", [[<cmd>lua require('substitute').visual()<cr>]], { noremap = true })
-    vim.keymap.set("n", "R", [["*<cmd>lua require('substitute').operator()<cr>]], { noremap = true })
-    vim.keymap.set("x", "R", [["*<cmd>lua require('substitute').visual()<cr>]], { noremap = true })
+    vim.keymap.set("n", "r", [[<cmd>lua require('substitute').operator()<cr>]], {})
+    vim.keymap.set("x", "r", [[<cmd>lua require('substitute').visual()<cr>]], {})
+    vim.keymap.set("n", "R", [["*<cmd>lua require('substitute').operator()<cr>]], {})
+    vim.keymap.set("x", "R", [["*<cmd>lua require('substitute').visual()<cr>]], {})
 
-    vim.keymap.set("n", "cx", "<cmd>lua require('substitute.exchange').operator()<cr>", { noremap = true })
-    vim.keymap.set("n", "cxx", "<cmd>lua require('substitute.exchange').line()<cr>", { noremap = true })
-    vim.keymap.set("x", "C", "<cmd>lua require('substitute.exchange').visual()<cr>", { noremap = true })
-    vim.keymap.set("n", "cxc", "<cmd>lua require('substitute.exchange').cancel()<cr>", { noremap = true })
+    vim.keymap.set("n", "cx", "<cmd>lua require('substitute.exchange').operator()<cr>", {})
+    vim.keymap.set("n", "cxx", "<cmd>lua require('substitute.exchange').line()<cr>", {})
+    vim.keymap.set("x", "C", "<cmd>lua require('substitute.exchange').visual()<cr>", {})
+    vim.keymap.set("n", "cxc", "<cmd>lua require('substitute.exchange').cancel()<cr>", {})
   end,
 }
