bc6aa389c08aa02675c31b3b565f56b20b605874
Kevin Traver <kevintraver@kevintraver.com>
2023-08-14 20:56:16 -0600

    set text-case keymap for snake case

--- nvim/lua/plugins/text-case.lua
+++ nvim/lua/plugins/text-case.lua
@@ -2,5 +2,6 @@ return {
   "johmsalas/text-case.nvim",
   config = function()
     require("textcase").setup()
+    vim.keymap.set("n", "gas", "<cmd>lua require('textcase').current_word('to_snake_case')<CR>", {})
   end,
 }
