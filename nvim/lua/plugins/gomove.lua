523da666c85e2f556e762f9a2fde1a0ee2ba2e8a
Kevin Traver <kevintraver@kevintraver.com>
2023-08-14 19:50:35 -0600

    set l,h move keymaps to move lines instead of characters

--- nvim/lua/plugins/gomove.lua
+++ nvim/lua/plugins/gomove.lua
@@ -2,5 +2,7 @@ return {
   "booperlv/nvim-gomove",
   config = function()
     require("gomove").setup()
+    vim.keymap.set("n", "<A-h>", "<cmd>lua require('gomove.mappings.base').MoveLineHorizontal(-vim.v.count1)<CR>", {})
+    vim.keymap.set("n", "<A-l>", "<cmd>lua require('gomove.mappings.base').MoveLineHorizontal(vim.v.count1)<CR>", {})
   end,
 }
