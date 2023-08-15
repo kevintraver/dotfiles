27616f3dc69dab35e18c6a18224b1b271f886310
Kevin Traver <kevintraver@kevintraver.com>
2023-08-15 08:20:48 -0600

    flash: add linewise jump

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -18,5 +18,18 @@ return {
       end,
       desc = "Flash",
     },
+
+    {
+      "j",
+      mode = { "o", "x" },
+      function()
+        require("flash").jump({
+          search = { mode = "search", max_length = 0 },
+          label = { after = { 0, 0 } },
+          pattern = "^",
+        })
+      end,
+      desc = "Flash",
+    },
   },
 }
