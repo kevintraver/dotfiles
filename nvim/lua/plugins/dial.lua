55fef98030bff518e4887c171dfe2634cd4e495b
Kevin Traver <kevintraver@kevintraver.com>
2023-08-15 23:16:57 -0600

    dial: add groups

--- nvim/lua/plugins/dial.lua
+++ nvim/lua/plugins/dial.lua
@@ -3,6 +3,13 @@
 return {
   "monaqa/dial.nvim",
   config = function()
+    local augend = require("dial.augend")
+    require("dial.config").augends:register_group({
+      default = {
+        augend.integer.alias.decimal,
+        augend.constant.alias.bool,
+      },
+    })
     vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal(), {})
     vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal(), {})
     vim.keymap.set("n", "g<C-a>", require("dial.map").inc_gnormal(), {})
