5270c5f57cc95bc23e2bb985d2aa9d63b824cdc0
Kevin Traver <kevintraver@kevintraver.com>
2023-08-17 13:06:52 -0600

    remove more various-textobj keymaps

--- nvim/lua/plugins/various-textobjs.lua
+++ nvim/lua/plugins/various-textobjs.lua
@@ -10,25 +10,17 @@ return {
   vim.keymap.set({ "o", "x" }, "iS", "<cmd>lua require('various-textobjs').subword(true)<CR>"),
   vim.keymap.set({ "o", "x" }, "aS", "<cmd>lua require('various-textobjs').subword(false)<CR>"),
 
-  vim.keymap.set({ "o" }, "l", "<cmd>lua require('various-textobjs').nearEoL()<CR>"),
-  vim.keymap.set({ "o" }, "h", "^"),
-
   vim.keymap.set({ "o" }, "L", "$"),
   vim.keymap.set({ "o" }, "H", "0"),
 
   vim.keymap.set({ "n", "x" }, "L", "$"),
   vim.keymap.set({ "n", "x" }, "H", "^"),
 
-  vim.keymap.set({ "o", "x" }, "il", "<cmd>lua require('various-textobjs').lineCharacterwise(true)<CR>"),
-  vim.keymap.set({ "o", "x" }, "al", "<cmd>lua require('various-textobjs').lineCharacterwise(false)<CR>"),
-
   vim.keymap.set({ "o", "x" }, "ik", "<cmd>lua require('various-textobjs').key(true)<CR>"),
   vim.keymap.set({ "o", "x" }, "ak", "<cmd>lua require('various-textobjs').key(false)<CR>"),
 
   vim.keymap.set({ "o", "x" }, "iv", "<cmd>lua require('various-textobjs').value(true)<CR>"),
   vim.keymap.set({ "o", "x" }, "av", "<cmd>lua require('various-textobjs').value(false)<CR>"),
-
-  vim.keymap.set({ "o", "x" }, "C", "<cmd>lua require('various-textobjs').toNextClosingBracket()<CR>"),
 
   vim.keymap.set({ "o", "x" }, "gG", "<cmd>lua require('various-textobjs').entireBuffer()<CR>"),
 }
