de57666df4169ec5d8483420cb9bce7d854c6a29
Kevin Traver <kevintraver@kevintraver.com>
2023-08-16 22:53:54 -0600

    add treesitter playground

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,10 @@
 
 return {
   "nvim-treesitter/nvim-treesitter",
-  dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/playground",
+  },
   build = function()
     require("nvim-treesitter.install").update({ with_sync = true })
   end,
@@ -49,6 +52,24 @@ return {
           swap_previous = {
             ["<A-,>"] = "@parameter.inner",
           },
+        },
+      },
+      playground = {
+        enable = not vim.g.vscode,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = false, -- Whether the query persists across vim sessions
+        keybindings = {
+          toggle_query_editor = "o",
+          toggle_hl_groups = "i",
+          toggle_injected_languages = "t",
+          toggle_anonymous_nodes = "a",
+          toggle_language_display = "I",
+          focus_language = "f",
+          unfocus_language = "F",
+          update = "R",
+          goto_node = "<cr>",
+          show_help = "?",
         },
       },
     })
