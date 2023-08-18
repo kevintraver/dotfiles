469e1e2e9c709db18ef4e2c8413f88aef5a604d5
Kevin Traver <kevintraver@kevintraver.com>
2023-08-18 07:44:55 -0600

    change function keymap to m

--- nvim/lua/plugins/mini-ai.lua
+++ nvim/lua/plugins/mini-ai.lua
@@ -33,7 +33,7 @@ return {
           a = { "@block.outer", "@conditional.outer", "@loop.outer", "@function.outer" },
           i = { "@block.inner", "@conditional.inner", "@loop.inner", "@function.inner" },
         }, {}),
-        f = gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+        m = gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
         c = gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
       },
     })
