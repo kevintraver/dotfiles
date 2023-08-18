02e19efc5e11535f2cd886e036071cebd5a522c9
Kevin Traver <kevintraver@kevintraver.com>
2023-08-17 21:48:27 -0600

    add mini.ai text object for line (L)

--- nvim/lua/plugins/mini-ai.lua
+++ nvim/lua/plugins/mini-ai.lua
@@ -10,6 +10,25 @@ return {
       custom_textobjects = {
         d = { "[%-%.]?%f[%d]%d+" },
 
+        L = function(ai_type)
+          local line_num = vim.fn.line(".")
+          local line = vim.fn.getline(line_num)
+          -- Select `\n` past the line for `a` to delete it whole
+          local from_col, to_col = 1, line:len()
+          if ai_type == "i" then
+            if line:len() == 0 then
+              -- Don't remove empty line
+              from_col, to_col = 0, 0
+            else
+              -- Ignore indentation for `i` textobject and don't remove `\n` past the line
+              from_col = line:match("^%s*()")
+              to_col = line:len()
+            end
+          end
+
+          return { from = { line = line_num, col = from_col }, to = { line = line_num, col = to_col } }
+        end,
+
         b = gen_spec.treesitter({
           a = { "@block.outer", "@conditional.outer", "@loop.outer", "@function.outer" },
           i = { "@block.inner", "@conditional.inner", "@loop.inner", "@function.inner" },
