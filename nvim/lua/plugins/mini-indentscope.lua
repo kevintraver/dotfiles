76202e8a8dc4d4e885ee71330a7576c5e0768937
Kevin Traver <kevintraver@kevintraver.com>
2023-08-18 07:20:43 -0600

    indentscope: dont draw in vscode

--- nvim/lua/plugins/mini-indentscope.lua
+++ nvim/lua/plugins/mini-indentscope.lua
@@ -4,6 +4,11 @@ return {
   "echasnovski/mini.indentscope",
   event = "VeryLazy",
   config = function()
-    require("mini.indentscope").setup()
+    local delay_value = vim.g.vscode and 10000000 or 100
+    require("mini.indentscope").setup({
+      draw = {
+        delay = delay_value,
+      },
+    })
   end,
 }
