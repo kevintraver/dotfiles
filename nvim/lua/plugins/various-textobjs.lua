39695e439b202b6f8c1f636bd7c8de65901421d3
Kevin Traver <kevintraver@kevintraver.com>
2023-08-25 15:11:25 -0600

    Switch back to various-textobjs.lua for indentation textobjects
    - Delete mini-indentscope.lua
    - Define new key mappings for indentation textobjects in various-textobjs.lua

--- nvim/lua/plugins/various-textobjs.lua
+++ nvim/lua/plugins/various-textobjs.lua
@@ -10,6 +10,11 @@ return {
   vim.keymap.set({ "o", "x" }, "iS", "<cmd>lua require('various-textobjs').subword(true)<CR>"),
   vim.keymap.set({ "o", "x" }, "aS", "<cmd>lua require('various-textobjs').subword(false)<CR>"),
 
+  vim.keymap.set({ "o", "x" }, "ii", "<cmd>lua require('various-textobjs').indentation('inner', 'inner')<CR>"),
+  vim.keymap.set({ "o", "x" }, "ai", "<cmd>lua require('various-textobjs').indentation('outer', 'inner')<CR>"),
+  vim.keymap.set({ "o", "x" }, "iI", "<cmd>lua require('various-textobjs').indentation('inner', 'outer')<CR>"),
+  vim.keymap.set({ "o", "x" }, "aI", "<cmd>lua require('various-textobjs').indentation('outer', 'outer')<CR>"),
+
   vim.keymap.set({ "o" }, "L", "$"),
   vim.keymap.set({ "o" }, "H", "0"),
 
