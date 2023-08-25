209f14a6f0a50ac0538bc7bd313316fb31f25830
Kevin Traver <kevintraver@kevintraver.com>
2023-08-24 21:59:09 -0600

    yanky: add keymap (to use with preserve_cursor_position)

--- nvim/lua/plugins/yanky.lua
+++ nvim/lua/plugins/yanky.lua
@@ -10,6 +10,8 @@ return {
       },
     })
 
+    vim.keymap.set({ "n", "x" }, "y", "<Plug>(YankyYank)")
+
     vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
     vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
     vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
