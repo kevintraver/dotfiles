a2b99ca9015c1acab308ae9cb0cdc6d447e0cce3
Kevin Traver <kevintraver@kevintraver.com>
2023-09-04 10:53:42 -0600

    neovim: text-case to_snake_case keymap is now default

--- nvim/lua/plugins/text-case.lua
+++ nvim/lua/plugins/text-case.lua
@@ -5,6 +5,5 @@ return {
   event = "VeryLazy",
   config = function()
     require("textcase").setup()
-    vim.keymap.set("n", "gas", "<cmd>lua require('textcase').current_word('to_snake_case')<CR>", {})
   end,
 }
