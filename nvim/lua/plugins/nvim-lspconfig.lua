dbe52a58f4932b792f07b2941c329067cd9f2651
Kevin Traver <kevintraver@kevintraver.com>
2023-09-10 09:59:07 -0600

    setup typescript lsp

---
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,40 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = { "jose-elias-alvarez/typescript.nvim" },
+  opts = {
+    -- make sure mason installs the server
+    servers = {
+      tsserver = {
+        keys = {
+          { "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", desc = "Organize Imports" },
+          { "<leader>cR", "<cmd>TypescriptRenameFile<CR>", desc = "Rename File" },
+        },
+        settings = {
+          typescript = {
+            format = {
+              indentSize = vim.o.shiftwidth,
+              convertTabsToSpaces = vim.o.expandtab,
+              tabSize = vim.o.tabstop,
+            },
+          },
+          javascript = {
+            format = {
+              indentSize = vim.o.shiftwidth,
+              convertTabsToSpaces = vim.o.expandtab,
+              tabSize = vim.o.tabstop,
+            },
+          },
+          completions = {
+            completeFunctionCalls = true,
+          },
+        },
+      },
+    },
+    setup = {
+      tsserver = function(_, opts)
+        require("typescript").setup({ server = opts })
+        return true
+      end,
+    },
+  },
+}
