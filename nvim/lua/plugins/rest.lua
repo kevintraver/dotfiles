bcc9dbbdd83023e53679c18fd3a0d56969e1b011
Kevin Traver <kevintraver@kevintraver.com>
2023-09-09 23:19:25 -0600

    set filetype to http for rest plugin

--- nvim/lua/plugins/rest.lua
+++ nvim/lua/plugins/rest.lua
@@ -2,6 +2,7 @@
 
 return {
   "rest-nvim/rest.nvim",
+  ft = { "http" },
   config = function()
     require("rest-nvim").setup()
     vim.keymap.set("n", "<leader>r", "<Plug>RestNvim<CR>", { desc = "Send HTTP Request" })
