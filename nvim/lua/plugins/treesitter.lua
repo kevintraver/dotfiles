f1aced9e16a81a4d6081e9e5a79e2557fd837153
Kevin Traver <kevintraver@kevintraver.com>
2023-09-09 18:21:49 -0600

    update treesitter config for lazyvim
    playground is now part of treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,72 +1,37 @@
--- https://github.com/nvim-treesitter/nvim-treesitter
-
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = "VeryLazy",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "RRethy/nvim-treesitter-textsubjects",
-    "nvim-treesitter/playground",
-  },
-  build = function()
-    require("nvim-treesitter.install").update({ with_sync = true })
-  end,
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "javascript",
-        "json",
-        "ruby",
-        "html",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-      },
-      highlight = {
-        enable = not vim.g.vscode,
-      },
-      textobjects = {
-        select = {
-          enable = true,
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ["<A-.>"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["<A-,>"] = "@parameter.inner",
-          },
-        },
-      },
-      textsubjects = {
+  opts = {
+    ensure_installed = {
+      "bash",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "ruby",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
+    textobjects = {
+      select = {
         enable = true,
-        prev_selection = ",", -- (Optional) keymap to select the previous selection
-        keymaps = {
-          ["<cr>"] = "textsubjects-smart",
-          [";"] = "textsubjects-container-outer",
-          ["i;"] = "textsubjects-container-inner",
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<A-.>"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<A-,>"] = "@parameter.inner",
         },
       },
-      playground = {
-        enable = not vim.g.vscode,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = false, -- Whether the query persists across vim sessions
-        keybindings = {
-          toggle_query_editor = "o",
-          toggle_hl_groups = "i",
-          toggle_injected_languages = "t",
-          toggle_anonymous_nodes = "a",
-          toggle_language_display = "I",
-          focus_language = "f",
-          unfocus_language = "F",
-          update = "R",
-          goto_node = "<cr>",
-          show_help = "?",
-        },
-      },
-    })
-  end,
+    },
+  },
 }
