93732d0594c347804f817c687af03b6e77472d0e
Kevin Traver <kevintraver@kevintraver.com>
2023-09-10 17:14:52 -0600

    add neo-tree keymaps

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+
+  keys = {
+    {
+      "<D-e>",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = require("lazyvim.util").get_root() })
+      end,
+      desc = "Explorer NeoTree (root dir)",
+    },
+    {
+      "<D-E>",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+  },
+}
