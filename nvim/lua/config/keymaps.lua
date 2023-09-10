3852fb73ae53465b0733e46120f495c397e222ec
Kevin Traver <kevintraver@kevintraver.com>
2023-09-10 17:25:45 -0600

    add save, close, and split keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- save file
+vim.keymap.set({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+
+-- windows
+vim.keymap.set("n", "<D-d>", "<C-W>v", { desc = "Split window right", remap = true })
+vim.keymap.set("n", "<D-w>", "<C-W>c", { desc = "Delete window", remap = true })
