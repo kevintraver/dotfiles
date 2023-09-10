83598fc9cb6ec9f05b3db09f8a49b51dde714c92
Kevin Traver <kevintraver@kevintraver.com>
2023-09-09 18:26:16 -0600

    add symbols-outline

---
+++ nvim/lua/plugins/symbols-outline.lua
@@ -0,0 +1,8 @@
+-- https://github.com/simrat39/symbols-outline.nvim
+
+return {
+  "simrat39/symbols-outline.nvim",
+  cmd = "SymbolsOutline",
+  keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+  config = true,
+}
