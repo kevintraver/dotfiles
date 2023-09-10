17b86cb5193f04005101d7d1abe0bd2f71fd32ee
Kevin Traver <kevintraver@kevintraver.com>
2023-09-09 18:03:42 -0600

    neogit - use telescope

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -8,9 +8,13 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
     "sindrets/diffview.nvim",
-    "ibhagwan/fzf-lua",
   },
   config = function()
-    require("neogit").setup()
+    require("neogit").setup({
+
+      telescope_sorter = function()
+        return require("telescope").extensions.fzf.native_fzf_sorter()
+      end,
+    })
   end,
 }
