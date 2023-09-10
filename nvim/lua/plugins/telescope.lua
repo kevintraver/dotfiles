112a8ed7fc53f2ce3df2bff56f19c10e64b65a81
Kevin Traver <kevintraver@kevintraver.com>
2023-09-09 18:05:59 -0600

    update telescope config to lazyvim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,14 +1,12 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.2",
-  cond = not vim.g.vscode,
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
-  config = function()
-    require("telescope").setup()
-    require("telescope").load_extension("fzf")
-
-    vim.keymap.set("n", "<A-o>", require("telescope.builtin").find_files, {})
-  end,
+  "telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
 }
