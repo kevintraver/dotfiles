ead62392b8fb5e302a33e3f39ab5970326b73bf5
Kevin Traver <kevintraver@kevintraver.com>
2023-09-11 15:15:18 -0600

    remove neogit telescope_sorter override

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -10,12 +10,7 @@ return {
     "sindrets/diffview.nvim",
   },
   config = function()
-    require("neogit").setup({
-
-      telescope_sorter = function()
-        return require("telescope").extensions.fzf.native_fzf_sorter()
-      end,
-    })
+    require("neogit").setup({})
 
     if not vim.g.vscode then
       local neogit = require("neogit")
