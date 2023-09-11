9f0e7ea10061f6c48a8590e426993e6661b2281b
Kevin Traver <kevintraver@kevintraver.com>
2023-09-11 15:37:37 -0600

    move treejs out of options to lazy keys

--- nvim/lua/plugins/treesj.lua
+++ nvim/lua/plugins/treesj.lua
@@ -4,12 +4,28 @@ return {
   "Wansmer/treesj",
   event = "VeryLazy",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  config = function()
-    require("treesj").setup({
-      use_default_keymaps = false,
-    })
-    vim.keymap.set({ "n" }, "gj", ":join<CR>", {})
-    vim.keymap.set({ "n" }, "gJ", ":join!<CR>", {})
-    vim.keymap.set({ "n" }, "J", "<cmd>lua require('treesj').toggle()<CR>", {})
-  end,
+
+  opts = {
+    use_default_keymaps = false,
+  },
+
+  keys = {
+    {
+      "gj",
+      ":join<CR>",
+      desc = "Join with spaces",
+    },
+    {
+      "gJ",
+      ":join!<CR>",
+      desc = "Join without spaces",
+    },
+    {
+      "J",
+      function()
+        require("treesj").toggle()
+      end,
+      desc = "Toggle split/join",
+    },
+  },
 }
