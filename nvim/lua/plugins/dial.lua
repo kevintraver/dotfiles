dd3fa5dfe6827ab39c1b279b95b923433a213ec2
Kevin Traver <kevintraver@kevintraver.com>
2023-09-11 15:48:22 -0600

    move dial keymaps from config to lazy keys

--- nvim/lua/plugins/dial.lua
+++ nvim/lua/plugins/dial.lua
@@ -11,13 +11,63 @@ return {
         augend.constant.alias.bool,
       },
     })
-    vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal(), {})
-    vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal(), {})
-    vim.keymap.set("n", "g<C-a>", require("dial.map").inc_gnormal(), {})
-    vim.keymap.set("n", "g<C-x>", require("dial.map").dec_gnormal(), {})
-    vim.keymap.set("v", "<C-a>", require("dial.map").inc_visual(), {})
-    vim.keymap.set("v", "<C-x>", require("dial.map").dec_visual(), {})
-    vim.keymap.set("v", "g<C-a>", require("dial.map").inc_gvisual(), {})
-    vim.keymap.set("v", "g<C-x>", require("dial.map").dec_gvisual(), {})
   end,
+  keys = {
+    {
+      "<C-a>",
+      function()
+        require("dial.map").inc_normal()
+      end,
+      desc = "Dial increment",
+    },
+    {
+      "<C-x>",
+      function()
+        require("dial.map").dec_normal()
+      end,
+      desc = "Dial decrement",
+    },
+    {
+      "g<C-a>",
+      function()
+        require("dial.map").inc_gnormal()
+      end,
+      desc = "Dial increment",
+    },
+    {
+      "g<C-x>",
+      function()
+        require("dial.map").dec_gnormal()
+      end,
+      desc = "Dial decrement",
+    },
+    {
+      "<C-a>",
+      function()
+        require("dial.map").inc_visual()
+      end,
+      desc = "Dial increment",
+    },
+    {
+      "<C-x>",
+      function()
+        require("dial.map").dec_visual()
+      end,
+      desc = "Dial decrement",
+    },
+    {
+      "g<C-a>",
+      function()
+        require("dial.map").inc_gvisual()
+      end,
+      desc = "Dial increment",
+    },
+    {
+      "g<C-x>",
+      function()
+        require("dial.map").dec_gvisual()
+      end,
+      desc = "Dial decrement",
+    },
+  },
 }
