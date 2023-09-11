4ba6672ee19ef61473e8d25c0b9808a09ecd5bce
Kevin Traver <kevintraver@kevintraver.com>
2023-09-10 18:13:18 -0600

    add neoai

---
+++ nvim/lua/plugins/neoai.lua
@@ -0,0 +1,28 @@
+-- https://github.com/Bryley/neoai.nvim
+
+return {
+  "Bryley/neoai.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = {
+    "NeoAI",
+    "NeoAIOpen",
+    "NeoAIClose",
+    "NeoAIToggle",
+    "NeoAIContext",
+    "NeoAIContextOpen",
+    "NeoAIContextClose",
+    "NeoAIInject",
+    "NeoAIInjectCode",
+    "NeoAIInjectContext",
+    "NeoAIInjectContextCode",
+  },
+  keys = {
+    { "<leader>as", desc = "summarize text" },
+    { "<leader>ag", desc = "generate git message" },
+  },
+  config = function()
+    require("neoai").setup({})
+  end,
+}
