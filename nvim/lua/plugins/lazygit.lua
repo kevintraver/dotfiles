10010e4810235c5cde1f864bbcd748dacbddd1ec
Kevin Traver <kevintraver@kevintraver.com>
2023-09-11 17:46:46 -0600

    dont need to call lazygit setup when using default options

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -4,9 +4,6 @@ return {
   "kdheepak/lazygit.nvim",
   cond = not vim.g.vscode,
   dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("lazy").setup({})
-  end,
   keys = {
     {
       "<leader>gg",
