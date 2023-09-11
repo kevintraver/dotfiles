2ad742152de160fd24b4f0fe29d5542cbefa353c
Kevin Traver <kevintraver@kevintraver.com>
2023-09-10 21:56:15 -0600

    add cmd-q quit keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,3 +8,5 @@ vim.keymap.set({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Sav
 -- windows
 vim.keymap.set("n", "<D-d>", "<C-W>v", { desc = "Split window right", remap = true })
 vim.keymap.set("n", "<D-w>", "<C-W>c", { desc = "Delete window", remap = true })
+
+vim.keymap.set("n", "<D-q>", "<cmd>qa<cr>", { desc = "Quit all" })
