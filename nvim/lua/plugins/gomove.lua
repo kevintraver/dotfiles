92b1de7732e4c20350d615d29b992670249012b9
Kevin Traver <kevintraver@kevintraver.com>
2023-09-11 11:46:23 -0600

    gomove: remove extra default keymaps

--- nvim/lua/plugins/gomove.lua
+++ nvim/lua/plugins/gomove.lua
@@ -3,9 +3,4 @@
 return {
   "booperlv/nvim-gomove",
   event = "VeryLazy",
-  config = function()
-    require("gomove").setup()
-    vim.keymap.set("n", "<A-h>", "<cmd>lua require('gomove.mappings.base').MoveLineHorizontal(-vim.v.count1)<CR>", {})
-    vim.keymap.set("n", "<A-l>", "<cmd>lua require('gomove.mappings.base').MoveLineHorizontal(vim.v.count1)<CR>", {})
-  end,
 }
