451988bc8523bd5523e26423dc40e2d8fa6c9f86
Kevin Traver <kevintraver@kevintraver.com>
2023-09-11 21:15:53 -0600

    dont remap keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,8 +6,8 @@
 vim.keymap.set({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
 -- windows
-vim.keymap.set("n", "<D-d>", "<C-W>v", { desc = "Split window right", remap = true })
-vim.keymap.set("n", "<D-w>", "<C-W>c", { desc = "Delete window", remap = true })
+vim.keymap.set("n", "<D-d>", "<C-W>v", { desc = "Split window right" })
+vim.keymap.set("n", "<D-w>", "<C-W>c", { desc = "Delete window" })
 
 vim.keymap.set("n", "<D-q>", "<cmd>qa<cr>", { desc = "Quit all" })
 
