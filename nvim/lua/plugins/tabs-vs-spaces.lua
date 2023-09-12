6863fd8a3a7c7c2d83761f740855c173a56718df
Kevin Traver <kevintraver@kevintraver.com>
2023-09-11 18:38:44 -0600

    tabs-vs-spaces use default opts

--- nvim/lua/plugins/tabs-vs-spaces.lua
+++ nvim/lua/plugins/tabs-vs-spaces.lua
@@ -4,7 +4,5 @@ return {
   "tenxsoydev/tabs-vs-spaces.nvim",
   event = "VeryLazy",
   cond = not vim.g.vscode,
-  config = function()
-    require("tabs-vs-spaces").setup()
-  end,
+  opts = {},
 }
