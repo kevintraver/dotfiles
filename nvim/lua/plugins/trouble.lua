a3c19b16316bfa0fee0d0b84440914ba38c8b2b2
Kevin Traver <kevintraver@kevintraver.com>
2023-09-11 23:30:40 -0600

    add trouble keymaps

---
+++ nvim/lua/plugins/trouble.lua
@@ -0,0 +1,43 @@
+-- https://github.com/folke/trouble.nvim
+
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    {
+      "<leader>xx",
+      function()
+        require("trouble").open()
+      end,
+      desc = "Trouble open",
+    },
+    {
+      "<leader>xw",
+      function()
+        require("trouble").open("workspace_diagnostics")
+      end,
+      desc = "Trouble open workspace diagnostics",
+    },
+    {
+      "<leader>xd",
+      function()
+        require("trouble").open("document_diagnostics")
+      end,
+      desc = "Trouble Open document diagnostics",
+    },
+    {
+      "<leader>xq",
+      function()
+        require("trouble").open("quickfix")
+      end,
+      desc = "Trouble Open quickfix",
+    },
+    {
+      "<leader>xl",
+      function()
+        require("trouble").open("loclist")
+      end,
+      desc = "Trouble Open location list",
+    },
+  },
+}
