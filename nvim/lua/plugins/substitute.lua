f154e6c68bccf1b010d5bf06178fef5dd5eda0f6
Kevin Traver <kevintraver@kevintraver.com>
2023-09-11 18:52:56 -0600

    move up substitute opts

--- nvim/lua/plugins/substitute.lua
+++ nvim/lua/plugins/substitute.lua
@@ -3,12 +3,13 @@
 return {
   "gbprod/substitute.nvim",
   event = "VeryLazy",
-  config = function()
-    require("substitute").setup({
-      on_substitute = require("yanky.integration").substitute(),
-      exchange = { use_esc_to_cancel = false },
-    })
 
+  opts = {
+    on_substitute = require("yanky.integration").substitute(),
+    exchange = { use_esc_to_cancel = false },
+  },
+
+  config = function()
     vim.keymap.set("n", "r", [[<cmd>lua require('substitute').operator()<cr>]], {})
     vim.keymap.set("x", "r", [[<cmd>lua require('substitute').visual()<cr>]], {})
     vim.keymap.set("n", "R", [["*<cmd>lua require('substitute').operator()<cr>]], {})
