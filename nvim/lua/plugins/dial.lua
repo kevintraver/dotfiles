2b8049ccaf78b293f11fab977c7543eb23e43754
Kevin Traver <kevintraver@kevintraver.com>
2023-09-11 21:06:16 -0600

    update dial keymap functions

--- nvim/lua/plugins/dial.lua
+++ nvim/lua/plugins/dial.lua
@@ -16,56 +16,60 @@ return {
     {
       "<C-a>",
       function()
-        require("dial.map").inc_normal()
+        require("dial.map").manipulate("increment", "normal")
       end,
       desc = "Dial increment",
     },
     {
       "<C-x>",
       function()
-        require("dial.map").dec_normal()
+        require("dial.map").manipulate("decrement", "normal")
       end,
       desc = "Dial decrement",
     },
     {
       "g<C-a>",
       function()
-        require("dial.map").inc_gnormal()
+        require("dial.map").manipulate("increment", "gnormal")
       end,
       desc = "Dial increment",
     },
     {
       "g<C-x>",
       function()
-        require("dial.map").dec_gnormal()
+        require("dial.map").manipulate("decrement", "gnormal")
       end,
       desc = "Dial decrement",
     },
     {
       "<C-a>",
+      mode = "v",
       function()
-        require("dial.map").inc_visual()
+        require("dial.map").manipulate("increment", "visual")
       end,
       desc = "Dial increment",
     },
     {
       "<C-x>",
+      mode = "v",
       function()
-        require("dial.map").dec_visual()
+        require("dial.map").manipulate("decrement", "visual")
       end,
       desc = "Dial decrement",
     },
     {
       "g<C-a>",
+      mode = "v",
       function()
-        require("dial.map").inc_gvisual()
+        require("dial.map").manipulate("increment", "gvisual")
       end,
       desc = "Dial increment",
     },
     {
       "g<C-x>",
+      mode = "v",
       function()
-        require("dial.map").dec_gvisual()
+        require("dial.map").manipulate("decrement", "gvisual")
       end,
       desc = "Dial decrement",
     },
