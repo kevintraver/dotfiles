e8dae9522779730e48c5a10723939f169cc067b1
Kevin Traver <kevintraver@kevintraver.com>
2023-09-12 19:25:10 -0600

    add back gomove keymaps

--- nvim/lua/plugins/gomove.lua
+++ nvim/lua/plugins/gomove.lua
@@ -2,5 +2,13 @@
 
 return {
   "booperlv/nvim-gomove",
-  event = "VeryLazy",
+  config = function()
+    require("gomove").setup()
+    vim.keymap.set("n", "<A-h>", function()
+      require("gomove.mappings.base").MoveLineHorizontal(-vim.v.count1)
+    end)
+    vim.keymap.set("n", "<A-l>", function()
+      require("gomove.mappings.base").MoveLineHorizontal(vim.v.count1)
+    end)
+  end,
 }
