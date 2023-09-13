7ba1ee85eefae8164d99b7456ea1881c46939505
Kevin Traver <kevintraver@kevintraver.com>
2023-09-13 01:39:08 -0600

    add flash jump line keymap

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -11,5 +11,16 @@ return {
       end,
       desc = "Flash Treesitter",
     },
+    {
+      mode = { "n", "o", "x" },
+      "<leader>j",
+      function()
+        require("flash").jump({
+          search = { mode = "search", max_length = 0 },
+          label = { after = { 0, 0 } },
+          pattern = "^",
+        })
+      end,
+    },
   },
 }
