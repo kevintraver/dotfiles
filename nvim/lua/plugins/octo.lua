a54fe2e6f3afb8105658d0c1ad2bb4f725287d21
Kevin Traver <kevintraver@kevintraver.com>
2023-09-13 17:40:04 -0600

    add octo plugin

---
+++ nvim/lua/plugins/octo.lua
@@ -0,0 +1,11 @@
+-- https://github.com/pwntester/octo.nvim
+
+return {
+  "pwntester/octo.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {},
+}
