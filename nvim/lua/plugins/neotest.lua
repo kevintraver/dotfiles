f7e43a5b4eea38b1dc1e88ee48b00a5ba1e09ef5
Kevin Traver <kevintraver@kevintraver.com>
2023-09-13 21:47:21 -0600

    add neotest

---
+++ nvim/lua/plugins/neotest.lua
@@ -0,0 +1,26 @@
+-- https://github.com/nvim-neotest/neotest
+
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "antoinemadec/FixCursorHold.nvim",
+    "rouge8/neotest-rust",
+  },
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        require("neotest-rust"),
+      },
+    })
+  end,
+  keys = {
+    {
+      "<leader>tr",
+      function()
+        require("neotest").run.run()
+      end,
+    },
+  },
+}
