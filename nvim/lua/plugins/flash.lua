dd815cac7edb3f5cf2e29ab92e83f917c6eeaac0
Kevin Traver <kevintraver@kevintraver.com>
2023-09-14 12:34:18 -0600

    update flash keymaps

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -4,12 +4,36 @@ return {
   "folke/flash.nvim",
   keys = {
     {
-      "<C-s>",
-      mode = { "n", "o", "x" },
+      "s",
+      mode = { "n" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "s",
+      mode = { "o", "x" },
       function()
         require("flash").treesitter()
       end,
       desc = "Flash Treesitter",
+    },
+    {
+      "<C-s>",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Flash Treesitter",
+    },
+    {
+      "S",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
     },
     {
       "<leader>j",
