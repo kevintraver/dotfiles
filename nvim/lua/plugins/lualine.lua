81ee4aea5f6b9e89a61bdf106610f2a522cc5d9f
Kevin Traver <kevintraver@kevintraver.com>
2023-09-18 12:28:18 -0600

    set lualine theme to tokyonight

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,11 @@
+-- https://github.com/nvim-lualine/lualine.nvim
+
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    theme = "tokyonight",
+  },
+}
