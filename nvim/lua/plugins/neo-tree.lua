59937f0a0c2b6565c7404cd142b59526ee22833b
Kevin Traver <kevintraver@kevintraver.com>
2023-09-17 22:05:42 -0600

    add neo-tree copy command
    copied from AstroNvim

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -13,6 +13,7 @@ return {
         ["<D-.>"] = "next_source",
         ["<Tab>"] = "prev_source",
         ["<S-Tab>"] = "next_source",
+        ["Y"] = "copy_selector",
       },
     },
     filesystem = {
@@ -26,6 +27,43 @@ return {
           ["<D-[>"] = "prev_git_modified",
         },
       },
+    },
+    commands = {
+      copy_selector = function(state)
+        local node = state.tree:get_node()
+        local filepath = node:get_id()
+        local filename = node.name
+        local modify = vim.fn.fnamemodify
+
+        local vals = {
+          ["BASENAME"] = modify(filename, ":r"),
+          ["EXTENSION"] = modify(filename, ":e"),
+          ["FILENAME"] = filename,
+          ["PATH (CWD)"] = modify(filepath, ":."),
+          ["PATH (HOME)"] = modify(filepath, ":~"),
+          ["PATH"] = filepath,
+          ["URI"] = vim.uri_from_fname(filepath),
+        }
+
+        local options = vim.tbl_filter(function(val)
+          return vals[val] ~= ""
+        end, vim.tbl_keys(vals))
+        if vim.tbl_isempty(options) then
+          return
+        end
+        table.sort(options)
+        vim.ui.select(options, {
+          prompt = "Choose to copy to clipboard:",
+          format_item = function(item)
+            return ("%s: %s"):format(item, vals[item])
+          end,
+        }, function(choice)
+          local result = vals[choice]
+          if result then
+            vim.fn.setreg('"', result)
+          end
+        end)
+      end,
     },
   },
 
