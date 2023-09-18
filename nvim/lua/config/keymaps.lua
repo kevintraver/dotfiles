d253194a4b7e59fbf77058fa251ba172a931bf91
Kevin Traver <kevintraver@kevintraver.com>
2023-09-17 18:58:26 -0600

    move unmaps to bottom

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -17,13 +17,13 @@ vim.keymap.set("n", "<D-S-q>", vim.cmd.Quit, { desc = "Quit! all" })
 vim.keymap.set("n", "<D-S-n>", vim.cmd.tabnew, { desc = "New tab" })
 vim.keymap.set("n", "<D-n>", vim.cmd.vnew, { desc = "New File" })
 
--- unmap lazyvim keymaps
-vim.keymap.set("n", "<leader>l", "")
-vim.keymap.set("n", "<leader>L", "")
-vim.keymap.set("n", "<leader><tab><tab>", "", { desc = "New Tab" })
-
 -- buffer
 vim.keymap.set("n", "<leader><space>", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 
 -- tabs
 vim.keymap.set("n", "<leader><tab>n", "<cmd>tabnew<cr>", { desc = "New Tab" })
+
+-- unmap lazyvim keymaps
+vim.keymap.set("n", "<leader>l", "")
+vim.keymap.set("n", "<leader>L", "")
+vim.keymap.set("n", "<leader><tab><tab>", "", { desc = "New Tab" })
