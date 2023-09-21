a8505f2d2568063af32e7da8596438b4c9a095c3
Kevin Traver <kevintraver@kevintraver.com>
2023-09-21 10:41:25 -0600

    add nvim-file-location plugin

---
+++ nvim/lua/plugins/file-location.lua
@@ -0,0 +1,22 @@
+-- https://github.com/diegoulloao/nvim-file-location
+
+return {
+  "diegoulloao/nvim-file-location",
+  opts = {
+    keymap = "<leader>yp",
+    add_line = false,
+  },
+  keys = {
+    {
+      "<leader>yp",
+      desc = "Copy file location (relative) to clipboard",
+    },
+    {
+      "<leader>yP",
+      function()
+        require("nvim-file-location").copy_file_location("absolute", false, false)
+      end,
+      desc = "Copy file location (absolute) to clipboard",
+    },
+  },
+}
