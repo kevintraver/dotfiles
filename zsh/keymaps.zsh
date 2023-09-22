7af92c5ab2cd9e12f6e0a721bf885c9af9809e9f
Kevin Traver <kevintraver@kevintraver.com>
2023-09-22 06:35:55 -0600

    add exit zsh keymap

--- zsh/keymaps.zsh
+++ zsh/keymaps.zsh
@@ -1,4 +1,11 @@
+# edit-command-line
 autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '^e' edit-command-line
-bindkey "\x1b[101;9u" edit-command-line
+bindkey '\x1b[101;9u' edit-command-line
+
+#exit
+exit_zsh() { exit 0 }
+zle -N exit_zsh
+bindkey '^w' exit_zsh
+bindkey '\x1b[119;9u' exit_zsh
