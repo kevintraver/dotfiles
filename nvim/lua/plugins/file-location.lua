4745541b5ba4cfbdfd29655225dbc1c8a4ba5d16
Kevin Traver <kevintraver@kevintraver.com>
2023-09-24 23:46:02 -0600

    update file-location keymap and description

--- nvim/lua/plugins/file-location.lua
+++ nvim/lua/plugins/file-location.lua
@@ -13,11 +13,11 @@ return {
       desc = "Copy file location (relative) to clipboard",
     },
     {
-      "<leader>yP",
+      "<leader>Yp",
       function()
         require("nvim-file-location").copy_file_location("absolute", false, false, "*")
       end,
-      desc = "Copy file location (absolute) to clipboard",
+      desc = "Copy file location (absolute) to system clipboard",
     },
   },
 }
