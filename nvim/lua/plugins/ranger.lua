9db3f24f4b05545c97e506d42ec5099e6ddc66ed
Kevin Traver <kevintraver@kevintraver.com>
2023-09-26 11:38:52 -0600

    set ranger ui

--- nvim/lua/plugins/ranger.lua
+++ nvim/lua/plugins/ranger.lua
@@ -3,6 +3,13 @@
 return {
   "kelly-lin/ranger.nvim",
   event = "VeryLazy",
+  opts = {
+    ui = {
+      border = "single",
+      height = 0.9,
+      width = 0.9,
+    },
+  },
   keys = {
     {
       "<D-r>",
