2db565b7c4900080b3759a482d189468262c1427
Kevin Traver <kevintraver@kevintraver.com>
2023-09-27 21:41:05 -0600

    disable autoformatting for .env files

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -8,3 +8,10 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = false
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
+  pattern = ".env*",
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
