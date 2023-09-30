31705d543dfaa3eacb7ccde3a4d828c35920ea91
Kevin Traver <kevintraver@kevintraver.com>
2023-09-29 20:58:58 -0600

    add treepin keymaps

--- nvim/lua/plugins/treepin.lua
+++ nvim/lua/plugins/treepin.lua
@@ -4,4 +4,20 @@ return {
   "KaitlynEthylia/TreePin",
   event = "VeryLazy",
   opts = {},
+  keys = {
+    {
+      "<leader>tp",
+      function()
+        require("treepin").pinLocal()
+      end,
+      { desc = "Pin Node" },
+    },
+    {
+      "<leader>tg",
+      function()
+        require("treepin").pinGo()
+      end,
+      { desc = "Go To Pin" },
+    },
+  },
 }
