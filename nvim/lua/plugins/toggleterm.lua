3dfd61cb41871e3fe56be05e4d2e7a478fc189a8
Kevin Traver <kevintraver@kevintraver.com>
2023-09-30 01:01:19 -0600

    use toggleterm for lazygit

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -53,5 +53,15 @@ return {
       end,
       desc = "Toggle (close) terminal",
     },
+    {
+      "<D-g>",
+      mode = { "n", "t" },
+      function()
+        require("toggleterm.terminal").Terminal
+          :new({ cmd = "lazygit", direction = "float", count = 5, hidden = true })
+          :toggle()
+      end,
+      desc = "Lazygit",
+    },
   },
 }
