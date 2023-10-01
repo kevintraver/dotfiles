a0424358526da23ce9311613e17918d65dc4bc73
Kevin Traver <kevintraver@kevintraver.com>
2023-10-01 14:55:15 -0600

    switch bookmarks plugin and update keymaps

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -4,6 +4,7 @@ return {
   "folke/which-key.nvim",
   opts = {
     defaults = {
+      ["<leader>m"] = { name = "+bookmarks" },
       ["<leader>g"] = { name = "+git" },
       ["<leader>r"] = { name = "+replace" },
     },
