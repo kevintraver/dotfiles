db8bfeec2eaa74cfcd05fb02d8461e0cc48b1657
Kevin Traver <kevintraver@kevintraver.com>
2023-10-01 14:55:15 -0600

    switch bookmarks plugin and update keymaps

--- nvim/lua/plugins/bookmarks.lua
+++ nvim/lua/plugins/bookmarks.lua
@@ -1,22 +1,70 @@
--- https://github.com/crusj/bookmarks.nvim
+-- https://github.com/tomasky/bookmarks.nvim
 
 return {
-  "crusj/bookmarks.nvim",
+  "tomasky/bookmarks.nvim",
   event = "VeryLazy",
-  cond = not vim.g.vscode,
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
+    "nvim-telescope/telescope.nvim",
+    config = function()
+      require("telescope").load_extension("bookmarks")
+    end,
   },
   opts = {
-    keymap = {
-      toggle = "<leader>bt",
-      add = "<leader>ba",
+    sign_priority = 8,
+    signs = {
+      add = { hl = "BookMarksAdd", text = "🏷️", numhl = "BookMarksAddNr", linehl = "BookMarksAddLn" },
+      ann = { hl = "BookMarksAnn", text = "📝", numhl = "BookMarksAnnNr", linehl = "BookMarksAnnLn" },
     },
   },
   keys = {
     {
-      "<leader>bt",
-      desc = "Toggle Bookmarks",
+      "ma",
+      function()
+        require("bookmarks").bookmark_ann()
+      end,
+      desc = "Bookmark (annotate)",
+    },
+    {
+      "mm",
+      function()
+        require("bookmarks").bookmark_toggle()
+      end,
+      desc = "Toggle Bookmark",
+    },
+    {
+      "<D-m>",
+      function()
+        require("bookmarks").bookmark_next()
+      end,
+      desc = "Bookmarks Next",
+    },
+    {
+      "<S-D-m>",
+      function()
+        require("bookmarks").bookmark_prev()
+      end,
+      desc = "Bookmarks Next",
+    },
+    {
+      "md",
+      function()
+        require("bookmarks").bookmark_clean()
+      end,
+      desc = "Clean Bookmarks (buffer)",
+    },
+    {
+      "mq",
+      function()
+        require("bookmarks").bookmark_list()
+      end,
+      desc = "Bookmarks List (quickfix)",
+    },
+    {
+      "ms",
+      function()
+        require("telescope").extensions.bookmarks.list({})
+      end,
+      desc = "Bookmarks Telescope",
     },
   },
 }
