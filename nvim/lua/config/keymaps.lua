bb947843fc25ea4f4ead9e3f370ce2a70943ac97
Kevin Traver <kevintraver@kevintraver.com>
2023-10-01 14:55:33 -0600

    update lazyvim delete keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -24,9 +24,9 @@ vim.keymap.set("n", "<leader><space>", "<cmd>e #<cr>", { desc = "Switch to Other
 -- tabs
 
 -- unmap lazyvim keymaps
-vim.keymap.set("n", "<leader>l", "")
-vim.keymap.set("n", "<leader>L", "")
-vim.keymap.set("n", "<leader><tab><tab>", "")
+vim.keymap.del("n", "<leader>l")
+vim.keymap.del("n", "<leader>L")
+vim.keymap.del("n", "<leader><tab><tab>")
 
 -- Use regular (not filtered) put in terminals
 vim.api.nvim_create_autocmd("TermEnter", {
