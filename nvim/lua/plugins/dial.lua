44633ba8ad7a098912079378b0db0f575e0ee868
Kevin Traver <kevintraver@kevintraver.com>
2023-10-01 23:39:36 -0600

    remove unused dial keymaps

--- nvim/lua/plugins/dial.lua
+++ nvim/lua/plugins/dial.lua
@@ -27,51 +27,5 @@ return {
       end,
       desc = "Dial decrement",
     },
-    {
-      "g<C-a>",
-      function()
-        require("dial.map").manipulate("increment", "gnormal")
-      end,
-      desc = "Dial increment",
-    },
-    {
-      "g<C-x>",
-      function()
-        require("dial.map").manipulate("decrement", "gnormal")
-      end,
-      desc = "Dial decrement",
-    },
-    {
-      "<C-a>",
-      mode = "v",
-      function()
-        require("dial.map").manipulate("increment", "visual")
-      end,
-      desc = "Dial increment",
-    },
-    {
-      "<C-x>",
-      mode = "v",
-      function()
-        require("dial.map").manipulate("decrement", "visual")
-      end,
-      desc = "Dial decrement",
-    },
-    {
-      "g<C-a>",
-      mode = "v",
-      function()
-        require("dial.map").manipulate("increment", "gvisual")
-      end,
-      desc = "Dial increment",
-    },
-    {
-      "g<C-x>",
-      mode = "v",
-      function()
-        require("dial.map").manipulate("decrement", "gvisual")
-      end,
-      desc = "Dial decrement",
-    },
   },
 }
