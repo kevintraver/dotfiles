92dbed0547e2e071193fc9368f54a40234e819ef
Kevin Traver <kevintraver@kevintraver.com>
2023-10-01 20:55:06 -0600

    change treejs load to InsertEnter

--- nvim/lua/plugins/treesj.lua
+++ nvim/lua/plugins/treesj.lua
@@ -2,9 +2,8 @@
 
 return {
   "Wansmer/treesj",
-  event = "VeryLazy",
+  event = "InsertEnter",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-
   opts = {
     use_default_keymaps = false,
   },
