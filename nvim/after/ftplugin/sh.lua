63f0f9444da4c452e943a417b9b9d64f55cd2340
Kevin Traver <kevintraver@kevintraver.com>
2023-10-03 15:32:35 -0600

    remove after ftplugin sh
    not needed

--- nvim/after/ftplugin/sh.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set({ "n", "i" }, "<C-Enter>", function()
-  require("toggleterm").send_lines_to_terminal("single_line", true, {})
-end, { desc = "Send line to terminal", buffer = true })
-
-vim.keymap.set({ "n", "i" }, "<D-Enter>", function()
-  require("toggleterm").send_lines_to_terminal("single_line", true, {})
-end, { desc = "Send line to terminal", buffer = true })
