8351009efaa533d09d11fb2388617c75fc9f7726
Kevin Traver <kevintraver@kevintraver.com>
2023-10-03 16:04:23 -0600

    fix bash and zsh after links

--- nvim/after/ftplugin/zsh.lua
+++ nvim/after/ftplugin/zsh.lua
@@ -1,1 +1,7 @@
-sh.lua
+vim.keymap.set({ "n", "i" }, "<C-Enter>", function()
+  require("toggleterm").send_lines_to_terminal("single_line", true, {})
+end, { desc = "Send line to terminal", buffer = true })
+
+vim.keymap.set({ "n", "i" }, "<D-Enter>", function()
+  require("toggleterm").send_lines_to_terminal("single_line", true, {})
+end, { desc = "Send line to terminal", buffer = true })
