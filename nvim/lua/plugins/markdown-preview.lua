69810d69167d34683e1eb6edbeccdc72a0144b50
Kevin Traver <kevintraver@kevintraver.com>
2023-10-03 22:06:57 -0600

    remove markdown previewers

--- nvim/lua/plugins/markdown-preview.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/iamcco/markdown-preview.nvim
-
-return {
-  "iamcco/markdown-preview.nvim",
-  event = "VeryLazy",
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
-}
