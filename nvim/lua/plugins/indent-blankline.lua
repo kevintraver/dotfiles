1b802c70e45344b1a069bab0409726058ec61491
Kevin Traver <kevintraver@kevintraver.com>
2023-10-04 18:29:56 -0600

    remove indent blankline setup
    use defaults from LazyVim

--- nvim/lua/plugins/indent-blankline.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/lukas-reineke/indent-blankline.nvim
-
-return {
-  "lukas-reineke/indent-blankline.nvim",
-  event = "VeryLazy",
-  config = function()
-    require("ibl").setup()
-  end,
-}
