296f0d8041e34d3dd40c9c21d63f882e4a95c7ed
Kevin Traver <kevintraver@kevintraver.com>
2023-10-12 16:57:17 -0600

    override edgy with toggleterm in right

--- nvim/lua/plugins/edgy.lua
+++ nvim/lua/plugins/edgy.lua
@@ -2,6 +2,44 @@ return {
   "folke/edgy.nvim",
   event = "VeryLazy",
   opts = {
+    right = {
+      {
+        ft = "toggleterm",
+        size = { width = 0.4 },
+        filter = function(buf, win)
+          return vim.api.nvim_win_get_config(win).relative == ""
+        end,
+      },
+    },
+    bottom = {
+      {
+        ft = "noice",
+        size = { height = 0.4 },
+        filter = function(buf, win)
+          return vim.api.nvim_win_get_config(win).relative == ""
+        end,
+      },
+      {
+        ft = "lazyterm",
+        title = "LazyTerm",
+        size = { height = 0.4 },
+        filter = function(buf)
+          return not vim.b[buf].lazyterm_cmd
+        end,
+      },
+      "Trouble",
+      { ft = "qf", title = "QuickFix" },
+      {
+        ft = "help",
+        size = { height = 20 },
+        -- don't open help files in edgy that we're editing
+        filter = function(buf)
+          return vim.bo[buf].buftype == "help"
+        end,
+      },
+      { ft = "spectre_panel", size = { height = 0.4 } },
+      { title = "Neotest Output", ft = "neotest-output-panel", size = { height = 15 } },
+    },
     animate = {
       enabled = false,
     },
