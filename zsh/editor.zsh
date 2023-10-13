d7423913ed35d4f389a1cb39dd1cb16b5a436244
Kevin Traver <kevintraver@kevintraver.com>
2023-10-12 22:47:54 -0600

    silence nvr "no server found" messages

--- zsh/editor.zsh
+++ zsh/editor.zsh
@@ -1,10 +1,10 @@
 if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
-  alias nvim="nvr -l --remote-wait +'set bufhidden=wipe'"
+  alias nvim="nvr -s -l --remote-wait +'set bufhidden=wipe'"
 fi
 
 if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
-  export VISUAL="nvr -l --remote-wait +'set bufhidden=wipe'"
-  export EDITOR="nvr -l --remote-wait +'set bufhidden=wipe'"
+  export VISUAL="nvr -s -l --remote-wait +'set bufhidden=wipe'"
+  export EDITOR="nvr -s -l --remote-wait +'set bufhidden=wipe'"
 else
   export VISUAL="nvim"
   export EDITOR="nvim"
