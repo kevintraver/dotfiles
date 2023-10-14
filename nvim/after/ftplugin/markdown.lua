7b9ecd7ae7da1be960cc5065347a932745db0a3d
Kevin Traver <kevintraver@kevintraver.com>
2023-10-14 16:47:09 -0600

    update Marked 2 open keymap

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("n", "<leader>pm", function()
+vim.keymap.set("n", "<leader>cp", function()
   vim.cmd("silent !open -a Marked\\ 2.app '%:p'")
 end, { desc = "Markdown Preview", buffer = true })
