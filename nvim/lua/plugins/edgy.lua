b74e9440e14b4975bd715f67a79ee6adde865dac
Kevin Traver <kevintraver@kevintraver.com>
2023-10-13 19:31:21 -0600

    override edgy layout

--- nvim/lua/plugins/edgy.lua
+++ nvim/lua/plugins/edgy.lua
@@ -2,16 +2,6 @@ return {
   "folke/edgy.nvim",
   event = "VeryLazy",
   opts = {
-    right = {
-      {
-        ft = "toggleterm",
-        title = "Terminal",
-        size = { width = 0.4 },
-        filter = function(buf, win)
-          return vim.api.nvim_win_get_config(win).relative == ""
-        end,
-      },
-    },
     bottom = {
       {
         ft = "noice",
@@ -38,8 +28,51 @@ return {
           return vim.bo[buf].buftype == "help"
         end,
       },
-      { ft = "spectre_panel", size = { height = 0.4 } },
+      { title = "Spectre", ft = "spectre_panel", size = { height = 0.4 } },
+    },
+    right = {
+      {
+        ft = "toggleterm",
+        size = { width = 0.4 },
+        filter = function(buf, win)
+          return vim.api.nvim_win_get_config(win).relative == ""
+        end,
+      },
+      { title = "Neotest Summary", ft = "neotest-summary" },
       { title = "Neotest Output", ft = "neotest-output-panel", size = { height = 15 } },
+    },
+    left = {
+      {
+        title = "Neo-Tree",
+        ft = "neo-tree",
+        filter = function(buf)
+          return vim.b[buf].neo_tree_source == "filesystem"
+        end,
+        pinned = true,
+        open = function()
+          vim.api.nvim_input("<esc><space>e")
+        end,
+        size = { height = 0.5 },
+      },
+      {
+        title = "Neo-Tree Git",
+        ft = "neo-tree",
+        filter = function(buf)
+          return vim.b[buf].neo_tree_source == "git_status"
+        end,
+        pinned = true,
+        open = "Neotree position=right git_status",
+      },
+      {
+        title = "Neo-Tree Buffers",
+        ft = "neo-tree",
+        filter = function(buf)
+          return vim.b[buf].neo_tree_source == "buffers"
+        end,
+        pinned = true,
+        open = "Neotree position=top buffers",
+      },
+      "neo-tree",
     },
     animate = {
       enabled = false,
