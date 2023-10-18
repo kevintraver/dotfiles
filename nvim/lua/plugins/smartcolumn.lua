6aca0f1231cc9855b000325c52abf99bc01e2bf4
Kevin Traver <kevintraver@kevintraver.com>
2023-10-18 10:30:18 -0600

    add smartcolumn

---
+++ nvim/lua/plugins/smartcolumn.lua
@@ -0,0 +1,12 @@
+return {
+  "m4xshen/smartcolumn.nvim",
+  opts = {
+    disabled_filetypes = {
+      "help",
+      "text",
+      "markdown",
+      "NeogitStatus",
+      "lazy",
+    },
+  },
+}
