c6f4a17df6171f0628a1a40acf0fa5dd71005192
Kevin Traver <kevintraver@kevintraver.com>
2023-10-20 00:09:24 -0600

    drop nvr bufhidden=wipe

--- zsh/editor.zsh
+++ zsh/editor.zsh
@@ -1,10 +1,10 @@
 if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
-  alias nvim="nvr -s -l --remote-wait +'set bufhidden=wipe'"
+  alias nvim="nvr -s -l --remote-wait"
 fi
 
 if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
-  export VISUAL="nvr -s -l --remote-wait +'set bufhidden=wipe'"
-  export EDITOR="nvr -s -l --remote-wait +'set bufhidden=wipe'"
+  export VISUAL="nvr -s -l --remote-wait"
+  export EDITOR="nvr -s -l --remote-wait"
 else
   export VISUAL="nvim"
   export EDITOR="nvim"
