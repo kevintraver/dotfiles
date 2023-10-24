7e6d0b93a8ab322c29cfbecb5fcb0c9dcfd6bca5
Kevin Traver <kevintraver@kevintraver.com>
2023-10-24 08:33:49 -0600

    oil: set custom keymaps

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,7 +1,25 @@
 return {
   "stevearc/oil.nvim",
   event = "VeryLazy",
-  config = true,
+  opts = {
+    use_default_keymaps = false,
+    keymaps = {
+      ["g?"] = "actions.show_help",
+      ["<CR>"] = "actions.select",
+      ["<C-l>"] = "actions.select_vsplit",
+      ["<C-t>"] = "actions.select_tab",
+      ["<C-p>"] = "actions.preview",
+      ["<S-p>"] = "actions.preview",
+      ["<D-h>"] = "actions.parent",
+      ["-"] = "actions.parent",
+      ["_"] = "actions.open_cwd",
+      ["`"] = "actions.cd",
+      ["~"] = "actions.tcd",
+      ["gs"] = "actions.change_sort",
+      ["gx"] = "actions.open_external",
+      ["g."] = "actions.toggle_hidden",
+    },
+  },
   keys = {
     {
       "<D-h>",
