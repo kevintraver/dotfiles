f0926f54cfd62678192dc52e4b148760c92a12fa
Kevin Traver <kevintraver@kevintraver.com>
2023-10-25 13:29:20 -0600

    tidy keymaps config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,23 +1,34 @@
 -- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Default keymaps that are always set:
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
 vim.keymap.set("n", ";", ":")
 
 -- save file
-vim.keymap.set({ "i", "x", "n", "s" }, "<D-s>", vim.cmd.write, { desc = "Save file" })
+vim.keymap.set({ "i", "x", "n", "s" }, "<D-s>", vim.cmd.write, {
+  desc = "Save file",
+})
 
 -- windows
-vim.keymap.set({ "n", "i" }, "<D-l>", vim.cmd.vsplit, { desc = "Split window vertical" })
-vim.keymap.set({ "n", "i" }, "<D-S-l>", vim.cmd.split, { desc = "Split window horizontal" })
+vim.keymap.set({ "n", "i" }, "<D-l>", vim.cmd.vsplit, {
+  desc = "Split window vertical",
+})
+vim.keymap.set({ "n", "i" }, "<D-S-l>", vim.cmd.split, {
+  desc = "Split window horizontal",
+})
 
 -- close/quit
 vim.keymap.set({ "n", "i", "x" }, "<D-w>", function()
   vim.cmd.close()
 end, { desc = "Close" })
 
-vim.keymap.set({ "n", "i", "x" }, "<D-q>", vim.cmd.quitall, { desc = "Quit all" })
-vim.keymap.set({ "n", "i", "x" }, "<D-S-q>", vim.cmd.Quit, { desc = "Quit! all" })
+vim.keymap.set({ "n", "i", "x" }, "<D-q>", vim.cmd.quitall, {
+  desc = "Quit all",
+})
+vim.keymap.set({ "n", "i", "x" }, "<D-S-q>", vim.cmd.Quit, {
+  desc = "Quit! all",
+})
 
 -- new
 vim.keymap.set("n", "<D-S-n>", vim.cmd.tabnew, { desc = "New tab" })
@@ -40,8 +51,14 @@ vim.keymap.del("n", "<leader>fT")
 -- Use regular (not filtered) put in terminals
 vim.api.nvim_create_autocmd("TermEnter", {
   callback = function(event)
-    vim.keymap.set("n", "p", "<Plug>(YankyPutAfter)", { buffer = event.buf, silent = true })
-    vim.keymap.set("n", "P", "<Plug>(YankyPutBefore)", { buffer = event.buf, silent = true })
+    vim.keymap.set("n", "p", "<Plug>(YankyPutAfter)", {
+      buffer = event.buf,
+      silent = true,
+    })
+    vim.keymap.set("n", "P", "<Plug>(YankyPutBefore)", {
+      buffer = event.buf,
+      silent = true,
+    })
   end,
 })
 
