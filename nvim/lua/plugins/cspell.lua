01731e3dca549ae4daa16c69d2e057cedccc0e94
Kevin Traver <kevintraver@kevintraver.com>
2023-11-08 14:30:49 -0800

    move cspell to none-ls dependencies

--- nvim/lua/plugins/cspell.lua
+++ nvim/lua/plugins/cspell.lua
@@ -1,36 +1,33 @@
 return {
-  {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
     "davidmh/cspell.nvim",
-    event = "VeryLazy",
   },
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      table.insert(
-        opts.sources,
-        require("cspell").diagnostics.with({
-          diagnostics_postprocess = function(diagnostic)
-            diagnostic.severity = vim.diagnostic.severity["WARN"]
-          end,
-        })
-      )
-      table.insert(opts.sources, require("cspell").code_actions)
+  opts = function(_, opts)
+    table.insert(
+      opts.sources,
+      require("cspell").diagnostics.with({
+        diagnostics_postprocess = function(diagnostic)
+          diagnostic.severity = vim.diagnostic.severity["WARN"]
+        end,
+      })
+    )
+    table.insert(opts.sources, require("cspell").code_actions)
 
-      local none_ls = require("null-ls")
-      local cspell_check_enabled = true
-      vim.keymap.set("n", "<leader>us", function()
-        if cspell_check_enabled then
-          none_ls.disable({ name = "cspell" })
-          cspell_check_enabled = false
-          vim.notify("CSpell disabled", vim.log.levels.WARN, { title = "CSpell" })
-        else
-          none_ls.enable({ name = "cspell" })
-          cspell_check_enabled = true
-          vim.notify("CSpell enabled", vim.log.levels.INFO, { title = "CSpell" })
-        end
-      end, { desc = "Toggle CSpell", noremap = true })
-    end,
-  },
+    local none_ls = require("null-ls")
+    local cspell_check_enabled = true
+    vim.keymap.set("n", "<leader>us", function()
+      if cspell_check_enabled then
+        none_ls.disable({ name = "cspell" })
+        cspell_check_enabled = false
+        vim.notify("CSpell disabled", vim.log.levels.WARN, { title = "CSpell" })
+      else
+        none_ls.enable({ name = "cspell" })
+        cspell_check_enabled = true
+        vim.notify("CSpell enabled", vim.log.levels.INFO, { title = "CSpell" })
+      end
+    end, { desc = "Toggle CSpell", noremap = true })
+  end,
   keys = {
     "<leader>us",
     desc = "Toggle CSpell",
