86bbf1e161123b2a2ff3eaa0f5160f688a7276e6
Kevin Traver <kevintraver@kevintraver.com>
2023-11-12 12:01:17 -0800

    add treesitter completion source

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -2,10 +2,12 @@ return {
   "hrsh7th/nvim-cmp",
   dependencies = {
     "chrisgrieser/cmp_yanky",
+    "ray-x/cmp-treesitter",
   },
   opts = function(_, opts)
     opts.sources = require("cmp").config.sources(vim.list_extend(opts.sources, {
       { name = "cmp_yanky" },
+      { name = "treesitter" },
     }))
   end,
 }
