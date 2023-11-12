a9b6b3de118e69ccab29620345f17f0b7989661b
Kevin Traver <kevintraver@kevintraver.com>
2023-11-12 11:36:00 -0800

    add yanky as completion source

---
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,11 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "chrisgrieser/cmp_yanky",
+  },
+  opts = function(_, opts)
+    opts.sources = require("cmp").config.sources(vim.list_extend(opts.sources, {
+      { name = "cmp_yanky" },
+    }))
+  end,
+}
