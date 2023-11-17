a52cb271345f51eb60d8943c785ed9ff9cb4e3da
Kevin Traver <kevintraver@kevintraver.com>
2023-11-16 19:53:32 -0800

    Don't lazy load comment.nvim

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,6 +1,6 @@
 return {
   "numToStr/Comment.nvim",
-  lazy = true,
+  lazy = false,
   opts = {
     pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
   },
