c3d909e6b3db48872a44f40bb0367fad26dc6d1f
Kevin Traver <kevintraver@kevintraver.com>
2023-11-18 11:23:24 -0800

    fix bufferline background color

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -2,5 +2,10 @@ return {
   "folke/tokyonight.nvim",
   opts = {
     style = "storm",
+    on_highlights = function(highlights, colors)
+      highlights.BufferLineFill = {
+        bg = colors.bg_dark,
+      }
+    end,
   },
 }
