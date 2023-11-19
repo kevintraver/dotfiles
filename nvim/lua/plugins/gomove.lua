4a0b7172852189a1aa1297553081989b8264e0d9
Kevin Traver <kevintraver@kevintraver.com>
2023-11-19 10:55:57 -0800

    move keymaps out of config to keys

--- nvim/lua/plugins/gomove.lua
+++ nvim/lua/plugins/gomove.lua
@@ -1,13 +1,20 @@
 return {
   "booperlv/nvim-gomove",
   event = "VeryLazy",
-  config = function()
-    require("gomove").setup()
-    vim.keymap.set("n", "<A-h>", function()
-      require("gomove.mappings.base").MoveLineHorizontal(-vim.v.count1)
-    end)
-    vim.keymap.set("n", "<A-l>", function()
-      require("gomove.mappings.base").MoveLineHorizontal(vim.v.count1)
-    end)
-  end,
+  keys = {
+    {
+      "<A-h>",
+      function()
+        require("gomove.mappings.base").MoveLineHorizontal(-vim.v.count1)
+      end,
+      desc = "Move line left",
+    },
+    {
+      "<A-l>",
+      function()
+        require("gomove.mappings.base").MoveLineHorizontal(vim.v.count1)
+      end,
+      desc = "Move line right",
+    },
+  },
 }
