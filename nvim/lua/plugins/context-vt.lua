4af182d519c1d66a2ee22f20c63c328a373bef85
Kevin Traver <kevintraver@kevintraver.com>
2023-11-25 11:57:00 -0800

    add keymap to togggle nvim_context_vt

--- nvim/lua/plugins/context-vt.lua
+++ nvim/lua/plugins/context-vt.lua
@@ -1,4 +1,20 @@
 return {
   "andersevenrud/nvim_context_vt",
   config = true,
+  keys = {
+    {
+      "<leader>uv",
+      function()
+        local Util = require("lazyvim.util")
+        local context_vt = require("nvim_context_vt")
+        context_vt.toggle_context()
+        if Util.inject.get_upvalue(context_vt.toggle_context, "enabled") then
+          Util.info("Enabled Virtual Text Context", { title = "Option" })
+        else
+          Util.warn("Disabled Virtual Text Context", { title = "Option" })
+        end
+      end,
+      desc = "Toggle Virtual Text Context",
+    },
+  },
 }
