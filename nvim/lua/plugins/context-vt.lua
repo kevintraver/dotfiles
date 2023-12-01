e3855d3943ea24824b5773d45252c8d22bb05e0b
Kevin Traver <kevintraver@kevintraver.com>
2023-12-01 11:32:06 -0800

    disable nvim_context_vt

--- nvim/lua/plugins/context-vt.lua
+++ nvim/lua/plugins/context-vt.lua
@@ -1,20 +1,5 @@
 return {
   "andersevenrud/nvim_context_vt",
+  enabled = false,
   config = true,
-  keys = {
-    {
-      "<leader>uv",
-      function()
-        local Util = require("lazyvim.util")
-        local context_vt = require("nvim_context_vt")
-        context_vt.toggle_context()
-        if Util.inject.get_upvalue(context_vt.toggle_context, "enabled") then
-          Util.info("Enabled Virtual Text Context", { title = "Option" })
-        else
-          Util.warn("Disabled Virtual Text Context", { title = "Option" })
-        end
-      end,
-      desc = "Toggle Virtual Text Context",
-    },
-  },
 }
