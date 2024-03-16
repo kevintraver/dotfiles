9a683a1c00a2ef6869bbaa0462605b4fc9e3fa4a
Kevin Traver <kevintraver@kevintraver.com>
2024-03-16 16:53:48 -0700

    add neoswap

--- nvim/lua/plugins/gomove.lua
+++ nvim/lua/plugins/gomove.lua
@@ -1,20 +1,4 @@
 return {
   "booperlv/nvim-gomove",
   event = "VeryLazy",
-  keys = {
-    {
-      "<A-h>",
-      function()
-        require("gomove.mappings.base").MoveLineHorizontal(-vim.v.count1)
-      end,
-      desc = "Move line left",
-    },
-    {
-      "<A-l>",
-      function()
-        require("gomove.mappings.base").MoveLineHorizontal(vim.v.count1)
-      end,
-      desc = "Move line right",
-    },
-  },
 }
