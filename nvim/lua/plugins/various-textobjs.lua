60317b288195058472221dade2e245e28ec1a42d
Kevin Traver <kevintraver@kevintraver.com>
2024-03-15 23:19:12 -0700

    remove various-textobjs url keymaps

--- nvim/lua/plugins/various-textobjs.lua
+++ nvim/lua/plugins/various-textobjs.lua
@@ -16,29 +16,5 @@ return {
         require("various-textobjs").subword("inner")
       end,
     },
-    {
-      "u",
-      mode = { "o", "x" },
-      function()
-        require("various-textobjs").url()
-      end,
-      desc = "Url",
-    },
-    {
-      "au",
-      mode = { "o", "x" },
-      function()
-        require("various-textobjs").url()
-      end,
-      desc = "Url",
-    },
-    {
-      "iu",
-      mode = { "o", "x" },
-      function()
-        require("various-textobjs").url()
-      end,
-      desc = "Url",
-    },
   },
 }
