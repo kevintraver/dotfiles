a2c1ddc1dd672ff92726f5ea218f06d33e637796
Kevin Traver <kevintraver@kevintraver.com>
2024-03-17 22:16:42 -0700

    add neoswap keymaps

--- nvim/lua/plugins/neoswap.lua
+++ nvim/lua/plugins/neoswap.lua
@@ -2,4 +2,20 @@ return {
   "ecthelionvi/neoswap.nvim",
   lazy = true,
   config = true,
+  keys = {
+    {
+      "<S-A-l>",
+      function()
+        require("NeoSwap").swap_next()
+      end,
+      desc = "Swap Word Right",
+    },
+    {
+      "<S-A-h>",
+      function()
+        require("NeoSwap").swap_prev()
+      end,
+      desc = "Swap Word Left",
+    },
+  },
 }
