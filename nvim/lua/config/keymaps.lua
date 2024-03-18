3edfc7bff9c3b897823ef4f6bce95c70ad726c90
Kevin Traver <kevintraver@kevintraver.com>
2024-03-17 23:47:15 -0700

    remove tab keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -19,15 +19,9 @@ vim.keymap.set({ "n", "i", "x" }, "<A-q>", vim.cmd.quitall, {
   desc = "Quit all",
 })
 
--- new
-vim.keymap.set("n", "<A-S-n>", vim.cmd.tabnew, { desc = "New tab" })
-vim.keymap.set("n", "<A-n>", vim.cmd.tabnew, { desc = "New Tab" })
-vim.keymap.set("n", "<leader><tab>n", vim.cmd.tabnew, { desc = "New Tab" })
-
 -- unmap lazyvim keymaps
 vim.keymap.del("n", "<leader>l")
 vim.keymap.del("n", "<leader>L")
-vim.keymap.del("n", "<leader><tab><tab>")
 
 vim.keymap.del("n", "<leader>ft")
 vim.keymap.del("n", "<leader>fT")
