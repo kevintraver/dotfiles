00ca4287941d656a6938d10032a70abf4627389b
Kevin Traver <kevintraver@kevintraver.com>
2024-03-23 09:55:17 -0700

    hide Copilot Chat help

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -4,6 +4,9 @@ return {
     lazy = true,
     branch = "canary",
     config = true,
+    opts = {
+      show_help = false,
+    },
     dependencies = {
       { "zbirenbaum/copilot.lua" },
       { "nvim-lua/plenary.nvim" },
