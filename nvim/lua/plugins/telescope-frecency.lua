5e3b22f3c650cd15572fc327dcd77cfddc87e59f
Kevin Traver <kevintraver@kevintraver.com>
2024-03-24 10:57:30 -0700

    add smart-open plugin

--- nvim/lua/plugins/telescope-frecency.lua
+++ nvim/lua/plugins/telescope-frecency.lua
@@ -5,14 +5,4 @@ return {
     require("telescope").load_extension("frecency")
   end,
   dependencies = { "kkharji/sqlite.lua" },
-  keys = {
-    {
-      "<A-o>",
-      mode = { "n", "i" },
-      function()
-        require("telescope").extensions.frecency.frecency()
-      end,
-      desc = "Recent",
-    },
-  },
 }
---
+++ nvim/lua/plugins/telescope-smart-open.lua
@@ -0,0 +1,20 @@
+return {
+  "danielfalk/smart-open.nvim",
+  config = function()
+    require("telescope").load_extension("smart_open")
+  end,
+  dependencies = {
+    "kkharji/sqlite.lua",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { "nvim-telescope/telescope-fzy-native.nvim" },
+  },
+  keys = {
+    {
+      "<A-o>",
+      function()
+        require("telescope").extensions.smart_open.smart_open()
+      end,
+      desc = "Smart Open",
+    },
+  },
+}
