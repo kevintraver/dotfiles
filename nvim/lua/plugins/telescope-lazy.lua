c4513c6e86d42af284f1d0ea163db092b89cd4b1
Kevin Traver <kevintraver@kevintraver.com>
2024-03-24 04:59:29 -0700

    update telescope-lazy grep keymap

--- nvim/lua/plugins/telescope-lazy.lua
+++ nvim/lua/plugins/telescope-lazy.lua
@@ -2,6 +2,15 @@ return {
   "tsakirist/telescope-lazy.nvim",
   lazy = true,
   config = function()
+    require("telescope").setup({
+      extensions = {
+        lazy = {
+          mappings = {
+            open_in_live_grep = "<C-s>",
+          },
+        },
+      },
+    })
     require("telescope").load_extension("lazy")
   end,
   keys = {
