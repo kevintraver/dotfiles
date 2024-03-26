7ee68f8e29ae2796dc3e37dc5fbb8aec0825b274
Kevin Traver <kevintraver@kevintraver.com>
2024-03-26 00:18:53 -0700

    remove unused telescope extensions

--- nvim/lua/plugins/telescope-frecency.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-telescope/telescope-frecency.nvim",
-  enabled = false,
-  lazy = true,
-  config = function()
-    require("telescope").load_extension("frecency")
-  end,
-  dependencies = { "kkharji/sqlite.lua" },
-}
