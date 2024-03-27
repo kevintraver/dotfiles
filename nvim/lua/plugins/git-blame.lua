79b9617a94ddd18856ba788735d00da138c44b90
Kevin Traver <kevintraver@kevintraver.com>
2024-03-26 17:51:04 -0700

    feat(git-blame): add git-blame plugin and setup keymaps

---
+++ nvim/lua/plugins/git-blame.lua
@@ -0,0 +1,30 @@
+return {
+  "f-person/git-blame.nvim",
+  event = "VeryLazy",
+  opts = {
+    enabled = false,
+  },
+  keys = {
+    {
+      "<leader>gzt",
+      function()
+        require("gitblame").toggle()
+      end,
+      desc = "Toggle Git Blame",
+    },
+    {
+      "<leader>gzo",
+      function()
+        require("gitblame").open_commit_url()
+      end,
+      desc = "Open Commit URL",
+    },
+    {
+      "<leader>gzc",
+      function()
+        require("gitblame").copy_sha_to_clipboard()
+      end,
+      desc = "Copy Commit SHA to Clipboard",
+    },
+  },
+}
