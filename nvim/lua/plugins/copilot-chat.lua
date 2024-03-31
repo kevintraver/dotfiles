71e0f4402a03da64aa47d343cf6a5809e826d87e
Kevin Traver <kevintraver@kevintraver.com>
2024-03-31 02:04:13 -0700

    chore(tidy): remove extra brackets

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -1,53 +1,51 @@
 return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    lazy = true,
-    branch = "canary",
-    config = true,
-    opts = {
-      show_help = false,
-      mappings = {
-        reset = {
-          normal = "<C-n>",
-          insert = "<C-n>",
-        },
+  "CopilotC-Nvim/CopilotChat.nvim",
+  lazy = true,
+  branch = "canary",
+  config = true,
+  opts = {
+    show_help = false,
+    mappings = {
+      reset = {
+        normal = "<C-n>",
+        insert = "<C-n>",
       },
     },
-    dependencies = {
-      { "zbirenbaum/copilot.lua" },
-      { "nvim-lua/plenary.nvim" },
+  },
+  dependencies = {
+    { "zbirenbaum/copilot.lua" },
+    { "nvim-lua/plenary.nvim" },
+  },
+  keys = {
+    {
+      mode = { "n", "v" },
+      "<A-\\>",
+      function()
+        require("CopilotChat").toggle()
+      end,
+      desc = "Toggle Copilot Chat",
     },
-    keys = {
-      {
-        mode = { "n", "v" },
-        "<A-\\>",
-        function()
-          require("CopilotChat").toggle()
-        end,
-        desc = "Toggle Copilot Chat",
-      },
-      {
-        mode = { "n", "v" },
-        "<Leader>\\e",
-        function()
-          local copilot_chat = require("CopilotChat")
-          local config = require("CopilotChat.config")
-          copilot_chat.ask(config.prompts.Explain.prompt)
-        end,
-        desc = "Copilot Chat - Explain Code",
-      },
-      {
-        mode = { "n", "v" },
-        "<Leader>\\g",
-        function()
-          local copilot_chat = require("CopilotChat")
-          local config = require("CopilotChat.config")
-          copilot_chat.ask(config.prompts.CommitStaged.prompt, {
-            selection = config.prompts.CommitStaged.selection,
-          })
-        end,
-        desc = "Copilot Chat - Commit Message",
-      },
+    {
+      mode = { "n", "v" },
+      "<Leader>\\e",
+      function()
+        local copilot_chat = require("CopilotChat")
+        local config = require("CopilotChat.config")
+        copilot_chat.ask(config.prompts.Explain.prompt)
+      end,
+      desc = "Copilot Chat - Explain Code",
+    },
+    {
+      mode = { "n", "v" },
+      "<Leader>\\g",
+      function()
+        local copilot_chat = require("CopilotChat")
+        local config = require("CopilotChat.config")
+        copilot_chat.ask(config.prompts.CommitStaged.prompt, {
+          selection = config.prompts.CommitStaged.selection,
+        })
+      end,
+      desc = "Copilot Chat - Commit Message",
     },
   },
 }
