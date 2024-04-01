eeeab4bc6379f56b7ffd781adf01d1ab46638492
Kevin Traver <kevintraver@kevintraver.com>
2024-03-31 23:47:56 -0700

    feat(neotree): show hidden files

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -18,6 +18,11 @@ return {
       },
     },
     filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
       window = {
         mappings = {
           ["<Esc>"] = "clear_filter",
