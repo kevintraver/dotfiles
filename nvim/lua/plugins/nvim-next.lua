50f99169f1a32ed1e37faa2b6883914651a5aa56
Kevin Traver <kevintraver@kevintraver.com>
2024-03-31 22:48:39 -0700

    feat: add nvim-next plugin for better repeatable movements

---
+++ nvim/lua/plugins/nvim-next.lua
@@ -0,0 +1,14 @@
+return {
+  "ghostbuster91/nvim-next",
+  config = function()
+    require("nvim-next").setup({
+      default_mappings = {
+        repeat_style = "original",
+      },
+      items = {
+        require("nvim-next.builtins").f,
+        require("nvim-next.builtins").t,
+      },
+    })
+  end,
+}
