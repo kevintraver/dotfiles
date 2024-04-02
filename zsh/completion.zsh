9bf9002e26de38ecbe4526b0eaa3b6592e524025
Kevin Traver <kevintraver@kevintraver.com>
2024-04-02 04:40:06 -0700

    fix(zsh): update completion matchers

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,4 +1,5 @@
 zstyle ':completion:*' matcher-list '' \
   'm:{a-z\-}={A-Z\_}' \
   'r:[^[:alpha:]]||[[:alpha:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
-  'r:|?=** m:{a-z\-}={A-Z\_}'
+  'r:|?=** m:{a-z\-}={A-Z\_}' \
+  'm:{a-zA-Z}={A-Za-z}'
