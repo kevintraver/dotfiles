c609cc5b956267fcf61204424e85321096a97748
Kevin Traver <kevintraver@kevintraver.com>
2024-04-04 13:33:57 -0700

    fix(rayso): change keymap and update description

--- nvim/lua/plugins/rayso.lua
+++ nvim/lua/plugins/rayso.lua
@@ -1,14 +1,14 @@
 return {
   "TobinPalmer/rayso.nvim",
-  lazy = true,
   keys = {
     {
-      "<leader>cs",
+      "<leader>cp",
       mode = "v",
       function()
         ---@diagnostic disable-next-line: missing-parameter
         require("lib.create").create_snippet()
       end,
+      desc = "Create Snipper",
     },
   },
 }
