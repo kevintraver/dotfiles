1a23ae63eae5d873d0c525ff4253ad332db4f5f1
Kevin Traver <kevintraver@kevintraver.com>
2024-04-04 22:21:36 -0700

    fix(treesitter-outer): set keymaps

--- nvim/lua/plugins/treesitter-outer.lua
+++ nvim/lua/plugins/treesitter-outer.lua
@@ -2,7 +2,7 @@ return {
   "Mr-LLLLL/treesitter-outer",
   dependencies = "nvim-treesitter/nvim-treesitter",
   opts = {
-    prev_outer_key = "]o",
-    next_outer_key = "[o",
+    prev_outer_key = "[<tab>",
+    next_outer_key = "]<tab>",
   },
 }
