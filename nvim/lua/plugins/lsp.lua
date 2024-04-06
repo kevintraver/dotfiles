d865362bc4288b2e35fb35c2249e6a9e4d16d9da
Kevin Traver <kevintraver@kevintraver.com>
2024-04-06 16:41:49 -0500

    fix(lsp): remove keymaps

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,22 +3,8 @@ return {
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     keys[#keys + 1] = { "K", false }
-    keys[#keys + 1] = {
-      "<A-k>",
-      function()
-        require("telescope.builtin").lsp_definitions({
-          reuse_win = true,
-        })
-      end,
-      desc = "Goto Definition",
-      has = "definition",
-    }
-    keys[#keys + 1] = {
-      "gk",
-      function()
-        vim.lsp.buf.hover()
-      end,
-      desc = "Hover",
-    }
+    keys[#keys + 1] = { "gd", false }
+    keys[#keys + 1] = { "gD", false }
+    keys[#keys + 1] = { "gr", false }
   end,
 }
