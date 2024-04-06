8b425408c11cabef0a96c59bebfcaad1ea365afe
Kevin Traver <kevintraver@kevintraver.com>
2024-04-06 10:57:40 -0500

    fix(yanky): add put filter keymaps

--- nvim/lua/plugins/yanky.lua
+++ nvim/lua/plugins/yanky.lua
@@ -20,5 +20,23 @@ return {
       [["+<Plug>(YankyYank)]],
       desc = "Yank to Global Clipboard",
     },
+    {
+      "<A-p>",
+      mode = { "n" },
+      function()
+        local yanky_wrappers = require("yanky.wrappers")
+        require("yanky").put("p", false, yanky_wrappers.linewise(yanky_wrappers.change("==")))
+      end,
+      desc = "Yanky Put After Filter",
+    },
+    {
+      "<S-A-p>",
+      mode = { "n" },
+      function()
+        local yanky_wrappers = require("yanky.wrappers")
+        require("yanky").put("P", false, yanky_wrappers.linewise(yanky_wrappers.change("==")))
+      end,
+      desc = "Yanky Put Before Filter",
+    },
   },
 }
