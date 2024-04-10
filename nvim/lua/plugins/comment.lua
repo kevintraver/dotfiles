649945d9abc7a38aa379f88d2fe63c303cdf884f
Kevin Traver <kevintraver@kevintraver.com>
2024-04-10 18:21:55 -0500

    fix(comment): remove deprecated pre_hook

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,7 +1,5 @@
 return {
   "numToStr/Comment.nvim",
   event = "VeryLazy",
-  opts = {
-    pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-  },
+  config = true,
 }
