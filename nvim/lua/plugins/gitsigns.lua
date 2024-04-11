45f1714abfd5ed2947528ed172d9f7485f4d907d
Kevin Traver <kevintraver@kevintraver.com>
2024-04-11 13:16:26 -0500

    fix(gitsigns): update git change keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,14 +3,14 @@ return {
   keys = {
     {
       mode = { "n" },
-      "<A-]>",
+      "]]",
       function()
         require("gitsigns").next_hunk()
       end,
       desc = "Next Hunk",
     },
     {
-      "<A-[>",
+      "[[",
       mode = { "n" },
       function()
         require("gitsigns").prev_hunk()
