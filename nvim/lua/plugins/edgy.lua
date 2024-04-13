f7fef75c28cd22a80a038c14872e3a944268f4d2
Kevin Traver <kevintraver@kevintraver.com>
2024-04-13 11:54:28 -0600

    fix(neo-tree): switch back to neo-tree

--- nvim/lua/plugins/edgy.lua
+++ nvim/lua/plugins/edgy.lua
@@ -113,6 +113,27 @@ return {
         title = "NvimTree",
         ft = "NvimTree",
       },
+      {
+        title = "Neo-Tree",
+        ft = "neo-tree",
+        filter = function(buf)
+          return vim.b[buf].neo_tree_source == "filesystem"
+        end,
+      },
+      {
+        title = "Neo-Tree Git",
+        ft = "neo-tree",
+        filter = function(buf)
+          return vim.b[buf].neo_tree_source == "git_status"
+        end,
+      },
+      {
+        title = "Neo-Tree Buffers",
+        ft = "neo-tree",
+        filter = function(buf)
+          return vim.b[buf].neo_tree_source == "buffers"
+        end,
+      },
     },
     animate = {
       enabled = false,
