766fa260ed9d2ff73fe5824da23e29f5df05b20a
Kevin Traver <kevintraver@kevintraver.com>
2024-04-13 11:01:36 -0600

    fix(telescope): update telescope to search all buffers

--- nvim/lua/plugins/scope.lua
+++ nvim/lua/plugins/scope.lua
@@ -1,5 +1,24 @@
 return {
   "tiagovla/scope.nvim",
   event = "VeryLazy",
-  config = true,
+  config = function()
+    require("scope").setup()
+    require("telescope").load_extension("scope")
+  end,
+  keys = {
+    {
+      "<leader>bf",
+      function()
+        require("telescope").extensions.scope.buffers()
+      end,
+      desc = "Buffers",
+    },
+    {
+      "<leader>fb",
+      function()
+        require("telescope").extensions.scope.buffers()
+      end,
+      desc = "Buffers",
+    },
+  },
 }
