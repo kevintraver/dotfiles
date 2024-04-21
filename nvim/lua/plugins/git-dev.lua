4fb7437ca7027db419e3e1808a128cf87f3424eb
Kevin Traver <kevintraver@kevintraver.com>
2024-04-20 19:55:56 -0700

    feat(git-dev): add git-dev plugin

---
+++ nvim/lua/plugins/git-dev.lua
@@ -0,0 +1,23 @@
+return {
+  "moyiz/git-dev.nvim",
+  event = "VeryLazy",
+  opts = {
+    repositories_dir = vim.fn.stdpath("data") .. "/git-dev",
+    ephemeral = false,
+    read_only = false,
+  },
+  keys = {
+    {
+      "<leader>gd",
+      function()
+        vim.ui.input({ prompt = "Repository name / URI: " }, function(input)
+          if not input or input == "" then
+            return
+          end
+          require("git-dev").open(input)
+        end)
+      end,
+      desc = "Open a remote git repository",
+    },
+  },
+}
