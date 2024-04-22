7fbea58c129c823fb7c94e9919c25d72465bb861
Kevin Traver <kevintraver@kevintraver.com>
2024-04-21 17:01:11 -0700

    fix(screenkey): add toggle keymap

--- nvim/lua/plugins/screenkey.lua
+++ nvim/lua/plugins/screenkey.lua
@@ -2,4 +2,13 @@ return {
   "NStefan002/screenkey.nvim",
   cmd = "Screenkey",
   config = true,
+  keys = {
+    {
+      "<leader>uk",
+      function()
+        require("screenkey").toggle()
+      end,
+      desc = "Toggle Screenkey",
+    },
+  },
 }
