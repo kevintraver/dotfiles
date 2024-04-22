27e6c76b2a881bb50f7f514eafefb91d9ddbbf79
Kevin Traver <kevintraver@kevintraver.com>
2024-04-21 23:09:13 -0700

    fix(dev-docs): lazy load and set commands

--- nvim/lua/plugins/nvim-dev-docs.lua
+++ nvim/lua/plugins/nvim-dev-docs.lua
@@ -1,4 +1,17 @@
 return {
   "luckasRanarison/nvim-devdocs",
   config = true,
+  lazy = true,
+  cmd = {
+    "DevdocsFetch",
+    "DevdocsInstall",
+    "DevdocsUninstall",
+    "DevdocsOpen",
+    "DevdocsOpenFloat",
+    "DevdocsOpenCurrent",
+    "DevdocsOpenCurrentFloat",
+    "DevdocsToggle",
+    "DevdocsUpdate",
+    "DevdocsUpdateAll",
+  },
 }
