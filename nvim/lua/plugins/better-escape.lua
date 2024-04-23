b92477fcd9729b79b1d8ee8e81d1c2ac063a41aa
Kevin Traver <kevintraver@kevintraver.com>
2024-04-23 12:20:36 -0700

    fix(better-escape): use default opts

--- nvim/lua/plugins/better-escape.lua
+++ nvim/lua/plugins/better-escape.lua
@@ -1,10 +1,5 @@
 return {
   "max397574/better-escape.nvim",
   event = "InsertEnter",
-  opts = {
-    mapping = { "jk", "jj" },
-    timeout = vim.o.timeoutlen,
-    clear_empty_lines = false,
-    keys = "<Esc>",
-  },
+  config = true,
 }
