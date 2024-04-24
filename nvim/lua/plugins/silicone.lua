ef8ea0fc2520f45743d5e1ee8875d3a7b09fadcc
Kevin Traver <kevintraver@kevintraver.com>
2024-04-24 03:51:14 -0700

    feat(silicone): add silicon plugin

---
+++ nvim/lua/plugins/silicone.lua
@@ -0,0 +1,17 @@
+return {
+  "michaelrommel/nvim-silicon",
+  lazy = true,
+  cmd = "Silicon",
+  opts = {
+    font = "Hack",
+    to_clipboard = true,
+  },
+  keys = {
+    {
+      "<leader>ci",
+      function()
+        require("silicon").start()
+      end,
+    },
+  },
+}
