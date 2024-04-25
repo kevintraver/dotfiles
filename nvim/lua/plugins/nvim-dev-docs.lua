52fb57e590340651e0ce2e99dad8ed57bf4a505a
Kevin Traver <kevintraver@kevintraver.com>
2024-04-25 05:00:38 -0700

    fix(dev-docs): set close keymaps

--- nvim/lua/plugins/nvim-dev-docs.lua
+++ nvim/lua/plugins/nvim-dev-docs.lua
@@ -1,6 +1,5 @@
 return {
   "luckasRanarison/nvim-devdocs",
-  config = true,
   lazy = true,
   cmd = {
     "DevdocsFetch",
@@ -13,5 +12,18 @@ return {
     "DevdocsToggle",
     "DevdocsUpdate",
     "DevdocsUpdateAll",
+  },
+  opts = {
+    after_open = function()
+      vim.keymap.set({ "n" }, "jk", function()
+        vim.cmd.close()
+      end, { buffer = true, desc = "Close" })
+      vim.keymap.set({ "n" }, "q", function()
+        vim.cmd.close()
+      end, { buffer = true, desc = "Close" })
+      vim.keymap.set({ "n" }, "<esc>", function()
+        vim.cmd.close()
+      end, { buffer = true, desc = "Close" })
+    end,
   },
 }
