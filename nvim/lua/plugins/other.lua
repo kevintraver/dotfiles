a5f6677bb3c1700c8d86d352834829d49fdece3b
Kevin Traver <kevintraver@kevintraver.com>
2024-04-26 19:33:45 -0700

    feat(other): add other plugin

---
+++ nvim/lua/plugins/other.lua
@@ -0,0 +1,46 @@
+return {
+  "rgroli/other.nvim",
+  name = "other-nvim",
+  lazy = true,
+  opts = {
+    showMissingFiles = false,
+    mappings = {
+      {
+        context = "test",
+        pattern = "(.*).ts",
+        target = "%1.test.ts",
+      },
+      {
+        context = "implementation",
+        pattern = "(.*).test.ts",
+        target = "%1.ts",
+      },
+      {
+        context = "test",
+        pattern = "(.*).js",
+        target = "%1.spec.js",
+      },
+      {
+        context = "implementation",
+        pattern = "(.*).spec.js",
+        target = "%1.js",
+      },
+    },
+  },
+  keys = {
+    {
+      "<C-t>",
+      function()
+        require("other-nvim").open()
+      end,
+      desc = "Other File",
+    },
+    {
+      "<leader>ta",
+      function()
+        require("other-nvim").open()
+      end,
+      desc = "Alternate File",
+    },
+  },
+}
