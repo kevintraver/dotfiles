701e0e00c7faf8227585299b70452532d608a8cb
Kevin Traver <kevintraver@kevintraver.com>
2024-04-28 06:40:42 -0700

    fix(nvim-coverage): set lazy load

--- nvim/lua/plugins/nvim-coverage.lua
+++ nvim/lua/plugins/nvim-coverage.lua
@@ -1,5 +1,16 @@
 return {
   "andythigpen/nvim-coverage",
+  lazy = true,
+  cmd = {
+    "Coverage",
+    "CoverageLoad",
+    "CoverageLoadLcov",
+    "CoverageShow",
+    "CoverageHide",
+    "CoverageToggle",
+    "CoverageClear",
+    "CoverageSummary",
+  },
   config = {
     signs = {
       covered = { priority = 80 }, -- use a higher value than diagnostics or gitsigns
