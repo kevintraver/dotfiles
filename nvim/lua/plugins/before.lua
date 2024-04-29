88b32211bbd35590d53dcb96ad3e75764428fe65
Kevin Traver <kevintraver@kevintraver.com>
2024-04-28 23:38:03 -0700

    add before plugin keymaps

--- nvim/lua/plugins/before.lua
+++ nvim/lua/plugins/before.lua
@@ -6,4 +6,20 @@ return {
     require("before").setup()
     require("telescope").load_extension("before")
   end,
+  keys = {
+    {
+      "<C-A-o>",
+      function()
+        require("before").jump_to_last_edit()
+      end,
+      desc = "Jump to last edit",
+    },
+    {
+      "<C-A-i>",
+      function()
+        require("before").jump_to_next_edit()
+      end,
+      desc = "Jump to next edit",
+    },
+  },
 }
