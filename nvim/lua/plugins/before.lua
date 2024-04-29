f1ed41286ae5f53c3cd53de22d7caad3e8f7a12b
Kevin Traver <kevintraver@kevintraver.com>
2024-04-28 23:29:28 -0700

    use VeryLazy loading for before plugin

--- nvim/lua/plugins/before.lua
+++ nvim/lua/plugins/before.lua
@@ -1,6 +1,7 @@
 return {
   "bloznelis/before.nvim",
   enabled = true,
+  event = "VeryLazy",
   config = function()
     require("before").setup()
     require("telescope").load_extension("before")
