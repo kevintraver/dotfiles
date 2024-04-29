c74352816fb37eb20fb4db60769e32c2dd7cdace
Kevin Traver <kevintraver@kevintraver.com>
2024-04-28 23:24:24 -0700

    add neotest toggle watch keymap

---
+++ nvim/lua/plugins/neotest.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-neotest/neotest",
+  keys = {
+    {
+      "<leader>tw",
+      function()
+        require("neotest").watch.toggle(vim.fn.expand("%"))
+      end,
+      desc = "Watch File",
+    },
+  },
+}
