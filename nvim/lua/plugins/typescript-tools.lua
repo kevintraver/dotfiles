f99f71439892e1b76caf2a6265d7815d889cd0b3
Kevin Traver <kevintraver@kevintraver.com>
2024-05-06 11:04:36 -0600

    enable typescript-tool code actions

--- nvim/lua/plugins/typescript-tools.lua
+++ nvim/lua/plugins/typescript-tools.lua
@@ -5,5 +5,11 @@ return {
     "nvim-lua/plenary.nvim",
     "neovim/nvim-lspconfig",
   },
-  config = true,
+  config = function()
+    require("typescript-tools").setup({
+      settings = {
+        expose_as_code_action = "all",
+      },
+    })
+  end,
 }
