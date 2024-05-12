ebe8b5b9ebcaa9b2015363fbc29c415d31e8a851
Kevin Traver <kevintraver@kevintraver.com>
2024-05-12 04:39:17 -0600

    update split command arguments

--- nvim/lua/plugins/focus.lua
+++ nvim/lua/plugins/focus.lua
@@ -24,7 +24,7 @@ return {
     {
       "<A-S-l>",
       function()
-        require("focus").split_command("l", "", true)
+        require("focus").split_command("l", "")
       end,
       desc = "Split Nicely (bufnew)",
     },
