632cb11484b9a28eb092b8baabfcf132816a2da4
Kevin Traver <kevintraver@kevintraver.com>
2024-05-12 17:46:49 -0600

    remove custom tokyonight style

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -2,7 +2,6 @@ return {
   "folke/tokyonight.nvim",
   event = "VeryLazy",
   opts = {
-    style = "storm",
     on_highlights = function(highlights, colors)
       highlights.BufferLineFill = {
         bg = colors.bg_dark,
