2eb18f6f85fb1bdb55f999a9d5354eb56e366286
Kevin Traver <kevintraver@kevintraver.com>
2024-05-14 02:42:50 -0600

    remove lsp key unmaps

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,8 +4,5 @@ return {
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     keys[#keys + 1] = { "K", false }
-    keys[#keys + 1] = { "gd", false }
-    keys[#keys + 1] = { "gD", false }
-    keys[#keys + 1] = { "gr", false }
   end,
 }
