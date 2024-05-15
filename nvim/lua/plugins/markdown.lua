5273a8db3622655504ae439bef9c6c40432687af
Kevin Traver <kevintraver@kevintraver.com>
2024-05-15 04:54:59 -0600

    add render markdown plugin

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,6 +1,24 @@
 return {
-  "tadmccorkle/markdown.nvim",
-  ft = "markdown",
-  lazy = true,
-  config = true,
+  {
+    "tadmccorkle/markdown.nvim",
+    ft = "markdown",
+    lazy = true,
+    config = true,
+  },
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    lazy = true,
+    ft = "markdown",
+    name = "render-markdown",
+    config = true,
+    keys = {
+      {
+        "<leader>um",
+        function()
+          require("render-markdown").toggle()
+        end,
+        desc = "Toggle Render Markdown",
+      },
+    },
+  },
 }
