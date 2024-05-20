d9757cfbd412dc7fb756be78cda53d2186915cf3
Kevin Traver <kevintraver@kevintraver.com>
2024-05-20 13:42:47 -0600

    add markdown-togglecheck

---
+++ nvim/lua/plugins/toggle-checkbox.lua
@@ -0,0 +1,20 @@
+return {
+  "nfrid/markdown-togglecheck",
+  dependencies = {
+    "nfrid/treesitter-utils",
+  },
+  ft = {
+    "markdown",
+  },
+  keys = {
+    {
+      "<C-t>",
+      function()
+        vim.go.operatorfunc = "v:lua.require'markdown-togglecheck'.toggle"
+        return "g@l"
+      end,
+      expr = true,
+      desc = "Toggle Checkbox",
+    },
+  },
+}
