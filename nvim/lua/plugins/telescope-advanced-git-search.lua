966ed174c9b66b7a0fa4cfb7104a880e42726074
Kevin Traver <kevintraver@kevintraver.com>
2024-05-23 16:20:52 -0600

    add advanced-git-search

---
+++ nvim/lua/plugins/telescope-advanced-git-search.lua
@@ -0,0 +1,32 @@
+return {
+  "aaronhallaert/advanced-git-search.nvim",
+  cmd = "AdvancedGitSearch",
+  lazy = true,
+  config = function()
+    require("telescope").setup({
+      extensions = {
+        advanced_git_search = {
+          diff_plugin = "diffview",
+          git_flags = { "--no-pager" },
+        },
+      },
+    })
+    require("telescope").load_extension("advanced_git_search")
+  end,
+  keys = {
+    {
+      "<leader>ghl",
+      function()
+        require("telescope").extensions.advanced_git_search.search_log_content()
+      end,
+      desc = "Git Search",
+    },
+    {
+      "<leader>ghf",
+      function()
+        require("telescope").extensions.advanced_git_search.search_log_content_file()
+      end,
+      desc = "Git Search",
+    },
+  },
+}
