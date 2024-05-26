9f3684b63b6cc2f43e873e8da46b9fd70ba3c61c
Kevin Traver <kevintraver@kevintraver.com>
2024-05-26 10:59:20 -0600

    tidy hlchunk opts

--- nvim/lua/plugins/hlchunk.lua
+++ nvim/lua/plugins/hlchunk.lua
@@ -2,9 +2,17 @@ return {
   "shellRaining/hlchunk.nvim",
   event = "VeryLazy",
   opts = {
-    chunk = { notify = false },
-    line_num = { enable = true },
-    blank = { enable = false },
-    indent = { enable = false },
+    chunk = {
+      notify = false,
+    },
+    line_num = {
+      enable = true,
+    },
+    blank = {
+      enable = false,
+    },
+    indent = {
+      enable = false,
+    },
   },
 }
