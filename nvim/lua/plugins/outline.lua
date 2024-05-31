a337c4431f9164e275bbe39d090bcf337b4fab17
Kevin Traver <kevintraver@kevintraver.com>
2024-05-31 07:00:51 -0700

    move outline edgy config

--- nvim/lua/plugins/outline.lua
+++ nvim/lua/plugins/outline.lua
@@ -1,14 +1,28 @@
 return {
-  "hedyhli/outline.nvim",
-  lazy = true,
-  config = true,
-  keys = {
-    {
-      "<A-i>",
-      function()
-        require("outline").toggle_outline()
-      end,
-      desc = "Toggle Outline",
+  {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    config = true,
+    keys = {
+      {
+        "<A-i>",
+        function()
+          require("outline").toggle_outline()
+        end,
+        desc = "Toggle Outline",
+      },
     },
+  },
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        title = "Outline",
+        ft = "Outline",
+        size = { width = 0.4 },
+      })
+    end,
   },
 }
