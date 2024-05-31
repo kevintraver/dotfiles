5782f995e0dc4575ce846a9995824ef355b41e10
Kevin Traver <kevintraver@kevintraver.com>
2024-05-31 09:33:03 -0700

    add neogit edgy config

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -1,35 +1,59 @@
 return {
-  "NeogitOrg/neogit",
-  lazy = true,
-  branch = "nightly",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
-    "sindrets/diffview.nvim",
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    branch = "nightly",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "sindrets/diffview.nvim",
+    },
+    opts = {
+      integrations = {
+        telescope = true,
+        diffview = true,
+      },
+      status = {
+        recent_commit_count = 20,
+      },
+    },
+    keys = {
+      {
+        "<leader>gn",
+        function()
+          require("neogit").open({ kind = "split" })
+        end,
+        desc = "Neogit Open",
+      },
+      {
+        "<leader>gC",
+        function()
+          require("neogit").open({ "commit" })
+        end,
+        desc = "Neogit Open",
+      },
+    },
   },
-  opts = {
-    integrations = {
-      telescope = true,
-      diffview = true,
-    },
-    status = {
-      recent_commit_count = 20,
-    },
-  },
-  keys = {
-    {
-      "<leader>gn",
-      function()
-        require("neogit").open({ kind = "split" })
-      end,
-      desc = "Neogit Open",
-    },
-    {
-      "<leader>gC",
-      function()
-        require("neogit").open({ "commit" })
-      end,
-      desc = "Neogit Open",
-    },
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      table.insert(opts.bottom, {
+        ft = "NeogitStatus",
+        size = { height = 0.6 },
+      })
+      table.insert(opts.bottom, {
+        ft = "NeogitPopup",
+        size = { height = 0.4 },
+      })
+      table.insert(opts.bottom, {
+        ft = "NeogitCommitMessage",
+        size = { height = 0.4 },
+      })
+      table.insert(opts.bottom, {
+        ft = "NeogitCommitView",
+        size = { height = 0.4 },
+      })
+    end,
   },
 }
