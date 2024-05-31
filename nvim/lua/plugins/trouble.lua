6a2cbfd001765a0ceaceb9ba8d042acc3f77b3f9
Kevin Traver <kevintraver@kevintraver.com>
2024-05-31 09:19:53 -0700

    add trouble edgy config

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,7 +1,21 @@
 return {
-  "folke/trouble.nvim",
-  event = "VeryLazy",
-  opts = {
-    auto_preview = false,
+  {
+    "folke/trouble.nvim",
+    event = "VeryLazy",
+    opts = {
+      auto_preview = false,
+    },
+  },
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.bottom = opts.bottom or {}
+      table.insert(opts.bottom, {
+        ft = "trouble",
+        title = "Trouble",
+        size = { height = 0.3 },
+      })
+    end,
   },
 }
