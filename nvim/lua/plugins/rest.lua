4355af8fe7d7f16a2c4778c03ec1502534480554
Kevin Traver <kevintraver@kevintraver.com>
2024-05-31 07:01:14 -0700

    move rest edgy config

--- nvim/lua/plugins/rest.lua
+++ nvim/lua/plugins/rest.lua
@@ -1,17 +1,31 @@
 return {
-  "rest-nvim/rest.nvim",
-  lazy = true,
-  ft = "http",
-  name = "rest-nvim",
-  config = true,
-  keys = {
-    {
-      "<C-Enter>",
-      desc = "Send HTTP Request",
+  {
+    "rest-nvim/rest.nvim",
+    lazy = true,
+    ft = "http",
+    name = "rest-nvim",
+    config = true,
+    keys = {
+      {
+        "<C-Enter>",
+        desc = "Send HTTP Request",
+      },
+      {
+        "<A-Enter>",
+        desc = "Send HTTP Request",
+      },
     },
-    {
-      "<A-Enter>",
-      desc = "Send HTTP Request",
-    },
+  },
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        ft = "httpResult",
+        title = "HTTP Result",
+        size = { width = 0.4 },
+      })
+    end,
   },
 }
