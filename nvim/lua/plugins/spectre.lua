8732a3a27b1311d178c87eedb8f27cefa848f21d
Kevin Traver <kevintraver@kevintraver.com>
2024-05-31 09:21:32 -0700

    add spectre edgy config

--- nvim/lua/plugins/spectre.lua
+++ nvim/lua/plugins/spectre.lua
@@ -1,34 +1,48 @@
 return {
-  "nvim-pack/nvim-spectre",
-  lazy = true,
-  keys = {
-    {
-      "<leader>rr",
-      function()
-        require("spectre").open()
-      end,
-      desc = "Replace in Files (Spectre)",
+  {
+
+    "nvim-pack/nvim-spectre",
+    lazy = true,
+    keys = {
+      {
+        "<leader>rr",
+        function()
+          require("spectre").open()
+        end,
+        desc = "Replace in Files (Spectre)",
+      },
+      {
+        "<leader>rb",
+        function()
+          require("spectre").open({ path = vim.fn.expand("%:t:p") })
+        end,
+        desc = "Replace in Buffer (Spectre)",
+      },
+      {
+        "<leader>rf",
+        function()
+          require("spectre").open_file_search()
+        end,
+        desc = "Replace in Files (Spectre)",
+      },
+      {
+        "<leader>rw",
+        function()
+          require("spectre").open_visual({ select_word = true })
+        end,
+        desc = "Replace Word (Spectre)",
+      },
     },
-    {
-      "<leader>rb",
-      function()
-        require("spectre").open({ path = vim.fn.expand("%:t:p") })
-      end,
-      desc = "Replace in Buffer (Spectre)",
-    },
-    {
-      "<leader>rf",
-      function()
-        require("spectre").open_file_search()
-      end,
-      desc = "Replace in Files (Spectre)",
-    },
-    {
-      "<leader>rw",
-      function()
-        require("spectre").open_visual({ select_word = true })
-      end,
-      desc = "Replace Word (Spectre)",
-    },
+  },
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      table.insert(opts.right, {
+        title = "Spectre",
+        ft = "spectre_panel",
+        size = { width = 0.4 },
+      })
+    end,
   },
 }
