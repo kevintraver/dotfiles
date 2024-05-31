acd806fe96740adf5d63a0757f64864fe9229ffb
Kevin Traver <kevintraver@kevintraver.com>
2024-05-31 13:54:57 -0700

    remove outline edgy config override

--- nvim/lua/plugins/outline.lua
+++ nvim/lua/plugins/outline.lua
@@ -1,33 +1,20 @@
 return {
-  {
-    "hedyhli/outline.nvim",
-    lazy = true,
-    opts = {
-      keymaps = {
-        down_and_jump = { "<C-n>", "J" },
-        up_and_jump = { "<C-p>", "K" },
-        toggle_preview = "<S-p>",
-      },
-    },
-    keys = {
-      {
-        "<A-i>",
-        function()
-          require("outline").toggle_outline()
-        end,
-        desc = "Toggle Outline",
-      },
+  "hedyhli/outline.nvim",
+  lazy = true,
+  opts = {
+    keymaps = {
+      down_and_jump = { "<C-n>", "J" },
+      up_and_jump = { "<C-p>", "K" },
+      toggle_preview = "<S-p>",
     },
   },
-  {
-    "folke/edgy.nvim",
-    optional = true,
-    opts = function(_, opts)
-      table.insert(opts.right, {
-        title = "Outline",
-        ft = "Outline",
-        size = { width = 0.4 },
-      })
-    end,
+  keys = {
+    {
+      "<A-i>",
+      function()
+        require("outline").toggle_outline()
+      end,
+      desc = "Toggle Outline",
+    },
   },
 }
