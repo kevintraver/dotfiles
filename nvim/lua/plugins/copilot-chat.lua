3163a3479f914c7af6c1d5c9f7844b6a0ab5b0ce
Kevin Traver <kevintraver@kevintraver.com>
2024-05-31 10:12:20 -0700

    add copilotchat edgy layout

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -1,63 +1,77 @@
 return {
-  "CopilotC-Nvim/CopilotChat.nvim",
-  lazy = true,
-  branch = "canary",
-  config = true,
-  opts = {
-    show_help = false,
-    auto_insert_mode = true,
-    mappings = {
-      reset = {
-        normal = "<C-n>",
-        insert = "<C-n>",
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    lazy = true,
+    branch = "canary",
+    config = true,
+    opts = {
+      show_help = false,
+      auto_insert_mode = true,
+      mappings = {
+        reset = {
+          normal = "<C-n>",
+          insert = "<C-n>",
+        },
+        accept_diff = {
+          normal = "<C-Enter>",
+          insert = "<C-Enter>",
+        },
+        yank_diff = {
+          normal = "<C-y>",
+        },
+        close = {
+          normal = "<A-w>",
+          insert = "<A-w>",
+        },
       },
-      accept_diff = {
-        normal = "<C-Enter>",
-        insert = "<C-Enter>",
+    },
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    keys = {
+      {
+        "<A-\\>",
+        mode = { "n", "v" },
+        function()
+          require("CopilotChat").toggle()
+        end,
+        desc = "Toggle Copilot Chat",
       },
-      yank_diff = {
-        normal = "<C-y>",
+      {
+        mode = { "n", "v" },
+        "<leader>ae",
+        function()
+          local copilot_chat = require("CopilotChat")
+          local config = require("CopilotChat.config")
+          copilot_chat.ask(config.prompts.Explain.prompt)
+        end,
+        desc = "Copilot Chat - Explain Code",
       },
-      close = {
-        normal = "<A-w>",
-        insert = "<A-w>",
+      {
+        mode = { "n", "v" },
+        "<leader>ac",
+        function()
+          local copilot_chat = require("CopilotChat")
+          local config = require("CopilotChat.config")
+          copilot_chat.ask(config.prompts.CommitStaged.prompt, {
+            selection = config.prompts.CommitStaged.selection,
+          })
+        end,
+        desc = "Copilot Chat - Generate Commit Message",
       },
     },
   },
-  dependencies = {
-    { "zbirenbaum/copilot.lua" },
-    { "nvim-lua/plenary.nvim" },
-  },
-  keys = {
-    {
-      "<A-\\>",
-      mode = { "n", "v" },
-      function()
-        require("CopilotChat").toggle()
-      end,
-      desc = "Toggle Copilot Chat",
-    },
-    {
-      mode = { "n", "v" },
-      "<leader>ae",
-      function()
-        local copilot_chat = require("CopilotChat")
-        local config = require("CopilotChat.config")
-        copilot_chat.ask(config.prompts.Explain.prompt)
-      end,
-      desc = "Copilot Chat - Explain Code",
-    },
-    {
-      mode = { "n", "v" },
-      "<leader>ac",
-      function()
-        local copilot_chat = require("CopilotChat")
-        local config = require("CopilotChat.config")
-        copilot_chat.ask(config.prompts.CommitStaged.prompt, {
-          selection = config.prompts.CommitStaged.selection,
-        })
-      end,
-      desc = "Copilot Chat - Generate Commit Message",
-    },
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        ft = "copilot-chat",
+        title = "Copilot Chat",
+        size = { width = 0.5 },
+      })
+    end,
   },
 }
