cc9ad06794dff9d3b574dd6ad399924cc102dc15
Kevin Traver <kevintraver@kevintraver.com>
2024-05-31 09:06:35 -0700

    add neotest edgy config

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,12 +1,26 @@
 return {
-  "nvim-neotest/neotest",
-  keys = {
-    {
-      "<leader>tw",
-      function()
-        require("neotest").watch.toggle(vim.fn.expand("%"))
-      end,
-      desc = "Watch File",
+  {
+    "nvim-neotest/neotest",
+    keys = {
+      {
+        "<leader>tw",
+        function()
+          require("neotest").watch.toggle(vim.fn.expand("%"))
+        end,
+        desc = "Watch File",
+      },
     },
+  },
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        ft = "neotest-output-panel",
+        title = "Neotest Output",
+        size = { height = 0.4 },
+      })
+    end,
   },
 }
