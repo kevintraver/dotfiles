0337277288d8871ea925a1d885aea7eae03cddf7
Kevin Traver <kevintraver@kevintraver.com>
2024-06-03 13:56:51 -0600

    set fuzzy_buffer to use all loaded buffers

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -15,7 +15,18 @@ return {
       { name = "nvim_lsp" },
       { name = "snippets" },
       { name = "cmp_yanky" },
-      { name = "fuzzy_buffer" },
+      {
+        name = "fuzzy_buffer",
+        option = {
+          get_bufnrs = function()
+            local bufs = {}
+            for _, win in ipairs(vim.api.nvim_list_wins()) do
+              bufs[vim.api.nvim_win_get_buf(win)] = true
+            end
+            return vim.tbl_keys(bufs)
+          end,
+        },
+      },
       { name = "fuzzy_path" },
       { name = "treesitter" },
       { name = "dotenv" },
