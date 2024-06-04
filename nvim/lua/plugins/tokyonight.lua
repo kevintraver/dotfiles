8f8675aa088f0db99ae371dd5b468d3b361fe020
Kevin Traver <kevintraver@kevintraver.com>
2024-06-04 08:42:42 -0600

    set tokyonight style to storm

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -2,6 +2,7 @@ return {
   "folke/tokyonight.nvim",
   event = "VeryLazy",
   opts = {
+    style = "storm",
     on_highlights = function(highlights, colors)
       highlights.BufferLineFill = {
         bg = colors.bg_dark,
