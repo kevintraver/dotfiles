400a5ce2cdc66529a7381e063cb25d380b3e6605
Kevin Traver <kevintraver@kevintraver.com>
2024-06-06 06:39:43 -0600

    already manage fugit2 dependencies

--- nvim/lua/plugins/fugit2.lua
+++ nvim/lua/plugins/fugit2.lua
@@ -5,15 +5,6 @@ return {
     width = 70,
     external_diffview = true,
   },
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "nvim-lua/plenary.nvim",
-    {
-      "chrisgrieser/nvim-tinygit",
-      dependencies = { "stevearc/dressing.nvim" },
-    },
-  },
   cmd = { "Fugit2", "Fugit2Diff", "Fugit2Graph" },
   keys = {
     {
