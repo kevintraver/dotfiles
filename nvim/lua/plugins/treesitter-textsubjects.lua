bc8eda45d57e07d41e79d94b4179e49249d81599
Kevin Traver <kevintraver@kevintraver.com>
2024-06-09 02:21:35 -0600

    remove y text subjects smart keymap

--- nvim/lua/plugins/treesitter-textsubjects.lua
+++ nvim/lua/plugins/treesitter-textsubjects.lua
@@ -7,7 +7,6 @@ return {
         enable = true,
         keymaps = {
           ["."] = "textsubjects-smart",
-          ["y"] = "textsubjects-smart",
           ["<Space>"] = "textsubjects-smart",
         },
       },
