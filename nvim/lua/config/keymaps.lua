e9ef9c2eb5dbb1492f8cb0f35bbc72bb98ebb9d2
Kevin Traver <kevintraver@kevintraver.com>
2024-06-09 04:35:47 -0600

    add <leader><space> keymap to switch to alternate buffer

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -27,6 +27,10 @@ vim.keymap.set({ "n", "i", "x" }, "<A-C-w>", function()
   vim.cmd.bdelete()
 end, { desc = "Close Buffer" })
 
+vim.keymap.set("n", "<leader><space>", function()
+  vim.cmd.buffer("#")
+end, { desc = "Switch to Alternate Buffer" })
+
 -- close
 vim.keymap.set({ "n", "i", "x" }, "<A-S-w>", function()
   vim.cmd.stopinsert()
@@ -76,7 +80,6 @@ vim.keymap.set("n", "<leader>er", function()
   end
 end, { desc = "Open/Edit HTTP Request" })
 
--- Switch Tabs
 vim.keymap.set("n", "<C-Space>", function()
   vim.cmd.tabn()
 end)
