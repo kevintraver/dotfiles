72c3b30201513c7dbe3b221d8dedbe1fb849a7b5
Kevin Traver <kevintraver@kevintraver.com>
2024-06-09 18:12:42 -0600

    remove quick yank keymap

--- nvim/lua/plugins/quickselect.lua
+++ nvim/lua/plugins/quickselect.lua
@@ -1,16 +1,5 @@
 return {
   "ausgefuchster/quickselect.nvim",
   event = "VeryLazy",
-  opts = {
-    keymap = {
-      {
-        mode = { "n" },
-        "<C-y>",
-        function()
-          require("quickselect").quick_yank()
-        end,
-        desc = "Quick yank",
-      },
-    },
-  },
+  config = true,
 }
