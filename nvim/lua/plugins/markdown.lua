8fb88b655a37d77fe2cf7e2463e0ee921359d831
Kevin Traver <kevintraver@kevintraver.com>
2024-06-14 23:00:17 -0600

    move peek to markdown plugin spec

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -39,6 +39,27 @@ return {
   },
 
   {
+    "toppair/peek.nvim",
+    lazy = true,
+    enabled = true,
+    ft = "markdown",
+    build = "deno task --quiet build:fast",
+    opts = {
+      app = "browser",
+    },
+    keys = {
+      {
+        "<leader>mp",
+        function()
+          require("peek").open()
+        end,
+        desc = "Markdown Preview",
+        ft = "markdown",
+      },
+    },
+  },
+
+  {
     "iamcco/markdown-preview.nvim",
     lazy = true,
     enabled = false,
