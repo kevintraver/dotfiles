063419a330121fa4664255adddd40f911c0a95bb
Kevin Traver <kevintraver@kevintraver.com>
2024-06-15 15:25:36 -0600

    lsp: add <A-.> code action keymap

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -6,5 +6,6 @@ return {
     keys[#keys + 1] = { "K", false }
     keys[#keys + 1] = { "<A-n>", false }
     keys[#keys + 1] = { "<A-p>", false }
+    keys[#keys + 1] = { "<A-.>", vim.lsp.buf.code_action }
   end,
 }
