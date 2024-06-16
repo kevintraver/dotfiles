d000da202ee8805980e8a8c8818dcc2421f1bbe2
Kevin Traver <kevintraver@kevintraver.com>
2024-06-16 00:49:35 -0600

    zsh: update DOTFILES_HOME

--- zsh/history/history.zsh
+++ zsh/history/history.zsh
@@ -1,4 +1,4 @@
-HISTORY_IGNORE=$(cat $DOTFILES_HOME/history/ignore_patterns.txt | tr '\n' '|')
+HISTORY_IGNORE=$(cat $DOTFILES_HOME/zsh/history/ignore_patterns.txt | tr '\n' '|')
 
 HISTFILESIZE=100000
 HISTSIZE=100000
