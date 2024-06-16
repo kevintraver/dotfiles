0c62ef9d41e2896a843a44f6555a285b4a950225
Kevin Traver <kevintraver@kevintraver.com>
2024-06-15 18:22:48 -0600

    nvim-bqf: disable default tab keymaps

--- nvim/lua/plugins/bqf.lua
+++ nvim/lua/plugins/bqf.lua
@@ -2,6 +2,10 @@ return {
   "kevinhwang91/nvim-bqf",
   event = "VeryLazy",
   opts = {
+    func_map = {
+      stoggleup = "",
+      stoggledown = "",
+    },
     auto_resize_height = true,
     preview = {
       winblend = 0,
