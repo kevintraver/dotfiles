a5a6dd6c9867522a4e8d146246d3fc8af8195133
Kevin Traver <kevintraver@kevintraver.com>
2024-06-15 22:21:09 -0600

    flash: update s in visual mode to be normal jump instead of treesitter

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -28,7 +28,7 @@ return {
     },
     {
       "s",
-      mode = { "o", "x" },
+      mode = { "o" },
       function()
         require("flash").treesitter({
           remote_op = {
@@ -37,6 +37,14 @@ return {
         })
       end,
       desc = "Flash Treesitter",
+    },
+    {
+      "s",
+      mode = { "x" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash Jump",
     },
     {
       "r",
