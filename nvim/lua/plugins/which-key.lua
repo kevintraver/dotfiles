2b3bea360c21c67751e2b09f14c2952f2b91e10a
Kevin Traver <kevintraver@kevintraver.com>
2024-06-16 13:04:51 -0600

    whichkey: remove custom defaults and use LazyVim defaults

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,16 +1,5 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  opts = {
-    defaults = {
-      ["<leader>m"] = { name = "+bookmarks" },
-      ["<leader>g"] = { name = "+git" },
-      ["<leader>r"] = { name = "+replace" },
-      ["<leader>h"] = { name = "+githunk" },
-      ["<leader>q"] = { name = "+recorder" },
-      ["<leader>d"] = { name = "+debug" },
-      ["<leader>l"] = { name = "+lazy" },
-      ["<leader>o"] = { name = "+overseer" },
-    },
-  },
+  opts = {},
 }
