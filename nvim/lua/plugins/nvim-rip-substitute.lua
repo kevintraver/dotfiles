4f4944f91fd7ebf0d8d1d7f5184bf69257c94660
Kevin Traver <kevintraver@kevintraver.com>
2024-06-17 14:04:40 -0600

    nvim-rip-substitute: add tab keymap to cycle inputs

--- nvim/lua/plugins/nvim-rip-substitute.lua
+++ nvim/lua/plugins/nvim-rip-substitute.lua
@@ -16,5 +16,23 @@ return {
       end,
       desc = "Rip Substitute",
     },
+    {
+      "<tab>",
+      mode = { "n", "i" },
+      function()
+        --- @class RipSubstituteState
+        --- @field cursorPositions table<number, number>
+        local state = require("rip-substitute.state").state
+        if state.cursorPositions == nil then
+          state.cursorPositions = { [1] = 0, [2] = 0 }
+        end
+        local cursorPos = vim.api.nvim_win_get_cursor(state.popupWinNr)
+        state.cursorPositions[cursorPos[1]] = cursorPos[2]
+        local line = cursorPos[1] == 1 and 2 or 1
+        local col = state.cursorPositions[line] or 0
+        vim.api.nvim_win_set_cursor(state.popupWinNr, { line, col })
+      end,
+      ft = "rip-substitute",
+    },
   },
 }
