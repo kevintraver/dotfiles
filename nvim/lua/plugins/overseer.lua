98474b26a759440f03d5c9f25f0d3acb25342a8e
Kevin Traver <kevintraver@kevintraver.com>
2024-06-29 04:26:49 -0600

    overseer: use LazyVim extra config

--- nvim/lua/plugins/overseer.lua
+++ nvim/lua/plugins/overseer.lua
@@ -1,54 +1,26 @@
 return {
-  {
-    "stevearc/overseer.nvim",
-    lazy = true,
-    cmd = {
-      "OverseerOpen",
-      "OverseerClose",
-      "OverseerToggle",
-      "OverseerSaveBundle",
-      "OverseerLoadBundle",
-      "OverseerDeleteBundle",
-      "OverseerRunCmd",
-      "OverseerRun",
-      "OverseerInfo",
-      "OverseerBuild",
-      "OverseerQuickAction",
-      "OverseerTaskAction",
-      "OverseerClearCache",
-    },
-    opts = {
-      strategy = {
-        "toggleterm",
-        direction = "vertical",
-      },
-    },
-    keys = {
-      {
-        "<leader>or",
-        function()
-          require("overseer").run_template()
-        end,
-        desc = "Run Task",
-      },
-      {
-        "<leader>ot",
-        function()
-          require("overseer").toggle()
-        end,
-        desc = "Toggle",
-      },
+  "stevearc/overseer.nvim",
+  lazy = true,
+  opts = {
+    strategy = {
+      "toggleterm",
+      direction = "vertical",
     },
   },
-  {
-    "folke/edgy.nvim",
-    optional = true,
-    opts = function(_, opts)
-      table.insert(opts.bottom, {
-        ft = "OverseerList",
-        title = "Overseer",
-        size = { height = 0.4 },
-      })
-    end,
+  keys = {
+    {
+      "<leader>or",
+      function()
+        require("overseer").run_template()
+      end,
+      desc = "Run Task",
+    },
+    {
+      "<leader>ot",
+      function()
+        require("overseer").toggle()
+      end,
+      desc = "Toggle",
+    },
   },
 }
