21d7d3864c6518cf289a58a6ee5f7e8f93d3e5fe
Kevin Traver <kevintraver@kevintraver.com>
2024-07-01 10:39:04 -0600

    markdown: move markdown-pdf to markdown config

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -60,6 +60,25 @@ return {
   },
 
   {
+    "arminveres/md-pdf.nvim",
+    lazy = true,
+    ft = "markdown",
+    opts = {
+      toc = false,
+    },
+    keys = {
+      {
+        "<leader>mP",
+        function()
+          require("md-pdf").convert_md_to_pdf()
+        end,
+        desc = "Markdown PDF",
+        ft = "markdown",
+      },
+    },
+  },
+
+  {
     "iamcco/markdown-preview.nvim",
     lazy = true,
     enabled = false,
