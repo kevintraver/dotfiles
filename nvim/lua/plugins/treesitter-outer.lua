fb3044fd26fc3b713a92e7daa63a3abfd1ebe0f8
Kevin Traver <kevintraver@kevintraver.com>
2024-07-01 11:00:11 -0600

    treesitter: combine treesitter plugins

--- nvim/lua/plugins/treesitter-outer.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "Mr-LLLLL/treesitter-outer",
-  lazy = true,
-  dependencies = "nvim-treesitter/nvim-treesitter",
-  opts = {
-    prev_outer_key = "[{",
-    next_outer_key = "]}",
-  },
-}
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,116 +1,149 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "LazyFile", "VeryLazy" },
-  opts = {
-    ensure_installed = {
-      "bash",
-      "c",
-      "css",
-      "diff",
-      "git_config",
-      "git_rebase",
-      "gitattributes",
-      "gitcommit",
-      "gitignore",
-      "go",
-      "gomod",
-      "gowork",
-      "gosum",
-      "html",
-      "http",
-      "java",
-      "javascript",
-      "jq",
-      "jsdoc",
-      "json",
-      "jsonc",
-      "lua",
-      "luadoc",
-      "luap",
-      "markdown",
-      "markdown_inline",
-      "mermaid",
-      "prisma",
-      "python",
-      "query",
-      "regex",
-      "ruby",
-      "rust",
-      "scss",
-      "sql",
-      "toml",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "yaml",
-    },
-    rainbow = {
-      enable = true,
-    },
-    incremental_selection = {
-      enable = false,
-    },
-    textobjects = {
-      swap = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "LazyFile", "VeryLazy" },
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "css",
+        "diff",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+        "html",
+        "http",
+        "java",
+        "javascript",
+        "jq",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "mermaid",
+        "prisma",
+        "python",
+        "query",
+        "regex",
+        "ruby",
+        "rust",
+        "scss",
+        "sql",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+      rainbow = {
+        enable = true,
+      },
+      incremental_selection = {
         enable = false,
-        swap_next = {
-          ["<S-l>"] = {
-            query = {
-              "@parameter.inner",
-              "@assignment.inner",
+      },
+      textobjects = {
+        swap = {
+          enable = false,
+          swap_next = {
+            ["<S-l>"] = {
+              query = {
+                "@parameter.inner",
+                "@assignment.inner",
+              },
+            },
+          },
+          swap_previous = {
+            ["<S-h>"] = {
+              query = {
+                "@parameter.inner",
+                "@assignment.inner",
+              },
             },
           },
         },
-        swap_previous = {
-          ["<S-h>"] = {
-            query = {
-              "@parameter.inner",
-              "@assignment.inner",
+        select = {
+          enable = true,
+        },
+        move = {
+          enable = true,
+          goto_next_start = {
+            ["}"] = {
+              query = {
+                "@parameter.outer",
+              },
+            },
+          },
+          goto_previous_start = {
+            ["{"] = {
+              query = {
+                "@parameter.outer",
+              },
             },
           },
         },
       },
-      select = {
+      context_commentstring = {
         enable = true,
-      },
-      move = {
-        enable = true,
-        goto_next_start = {
-          ["}"] = {
-            query = {
-              "@parameter.outer",
-            },
-          },
-        },
-        goto_previous_start = {
-          ["{"] = {
-            query = {
-              "@parameter.outer",
-            },
-          },
-        },
+        enable_autocmd = false,
       },
     },
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
+    keys = {
+      {
+        "<Leader>it",
+        function()
+          vim.treesitter.inspect_tree()
+        end,
+        desc = "Inspect Tree",
+      },
+      {
+        "<Leader>eq",
+        function()
+          vim.treesitter.query.edit()
+        end,
+        desc = "Edit Query",
+      },
     },
   },
-  keys = {
-    {
-      "<Leader>it",
-      function()
-        vim.treesitter.inspect_tree()
-      end,
-      desc = "Inspect Tree",
+
+  {
+    "RRethy/nvim-treesitter-textsubjects",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        textsubjects = {
+          enable = true,
+          keymaps = {
+            ["."] = "textsubjects-smart",
+          },
+        },
+      })
+    end,
+  },
+
+  {
+    "Mr-LLLLL/treesitter-outer",
+    lazy = true,
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    opts = {
+      prev_outer_key = "[{",
+      next_outer_key = "]}",
     },
-    {
-      "<Leader>eq",
-      function()
-        vim.treesitter.query.edit()
-      end,
-      desc = "Edit Query",
-    },
+  },
+
+  {
+    "andersevenrud/nvim_context_vt",
+    enabled = false,
+    config = true,
   },
 }
