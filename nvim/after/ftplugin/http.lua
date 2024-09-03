f3e66df3248cf4675625b8b6aa00436dacae8bd6
Kevin Traver <kevintraver@kevintraver.com>
2024-09-03 17:34:20 -0600

    kulala: move keymaps from ftplugin to kulala config

--- nvim/after/ftplugin/http.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set({ "n", "i" }, "<C-Enter>", function()
-  require("kulala").run()
-end, { desc = "Send HTTP Request", buffer = true })
-
-vim.keymap.set({ "n", "i" }, "<A-Enter>", function()
-  require("kulala").run()
-end, { desc = "Send HTTP Request", buffer = true })
