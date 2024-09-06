49daffee01f37c5bd1f6ae9ddc2c467cbba50bd0
Kevin Traver <kevintraver@kevintraver.com>
2024-09-06 16:06:19 -0600

    grugfar: remove custom keymaps

--- nvim/lua/plugins/grug-far.lua
+++ nvim/lua/plugins/grug-far.lua
@@ -2,20 +2,6 @@ return {
   {
     "MagicDuck/grug-far.nvim",
     cmd = "GrugFar",
-    opts = {
-      keymaps = {
-        replace = { n = "R" },
-        qflist = { n = "<C-q>" },
-        syncLocations = { n = "s" },
-        syncLine = { n = "l" },
-        close = { n = "q" },
-        historyOpen = { n = "o" },
-        historyAdd = { n = "a" },
-        refresh = { n = "r" },
-        gotoLocation = { n = "<enter>" },
-        pickHistoryEntry = { n = "<enter>" },
-      },
-    },
     keys = {
       {
         "<leader>fr",
