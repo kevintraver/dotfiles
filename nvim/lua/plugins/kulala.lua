f3b4b859eac9d1da11d69a2058cff8fea828d08f
Kevin Traver <kevintraver@kevintraver.com>
2024-10-12 15:27:19 -0700

    kulala: add lsp and formatter

--- nvim/lua/plugins/kulala.lua
+++ nvim/lua/plugins/kulala.lua
@@ -1,36 +1,62 @@
 return {
-  "mistweaverco/kulala.nvim",
-  ft = "http",
-  keys = {
-    {
-      ft = { "http" },
-      "<C-Enter>",
-      function()
-        require("kulala").run()
-      end,
-      desc = "Send the request",
+  {
+    "mistweaverco/kulala.nvim",
+    ft = "http",
+    keys = {
+      {
+        ft = { "http" },
+        "<C-Enter>",
+        function()
+          require("kulala").run()
+        end,
+        desc = "Send the request",
+      },
+      {
+        ft = { "http" },
+        "<A-Enter>",
+        function()
+          require("kulala").run()
+        end,
+        desc = "Send the request",
+      },
+      {
+        "<leader>Ry",
+        function()
+          require("kulala").copy()
+        end,
+        desc = "Copy request as curl",
+      },
+      {
+        "<leader>Rp",
+        function()
+          require("kulala").from_curl()
+        end,
+        desc = "Paste from curl",
+      },
     },
-    {
-      ft = { "http" },
-      "<A-Enter>",
-      function()
-        require("kulala").run()
-      end,
-      desc = "Send the request",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        kulala_ls = {},
+      },
     },
-    {
-      "<leader>Ry",
-      function()
-        require("kulala").copy()
-      end,
-      desc = "Copy request as curl",
-    },
-    {
-      "<leader>Rp",
-      function()
-        require("kulala").from_curl()
-      end,
-      desc = "Paste from curl",
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters = {
+        kulala = {
+          command = "kulala-fmt",
+          args = { "$FILENAME" },
+          stdin = false,
+        },
+      },
+      formatters_by_ft = {
+        http = { "kulala" },
+      },
     },
   },
 }
