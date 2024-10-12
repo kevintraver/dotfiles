9ec8212834f0e8f50e2cdb564b0321c32b805ea3
Kevin Traver <kevintraver@kevintraver.com>
2024-10-11 17:09:05 -0700

    nvim: toggle terminal after deleting zsh window

--- nvim/after/ftplugin/zsh.lua
+++ nvim/after/ftplugin/zsh.lua
@@ -4,14 +4,35 @@ vim.api.nvim_create_autocmd("BufEnter", {
     vim.keymap.set({ "n", "i" }, "<A-w>", function()
       vim.cmd.write({ bang = true })
       vim.cmd.bdelete()
+      require("toggleterm.terminal").Terminal
+        :new({
+          direction = "horizontal",
+          count = 2,
+          hidden = true,
+        })
+        :toggle()
     end, { buffer = true })
     vim.keymap.set({ "n", "i" }, "<A-Enter>", function()
       vim.cmd.write({ bang = true })
       vim.cmd.bdelete()
+      require("toggleterm.terminal").Terminal
+        :new({
+          direction = "horizontal",
+          count = 2,
+          hidden = true,
+        })
+        :toggle()
     end, { buffer = true })
     vim.keymap.set({ "n", "i" }, "<C-Enter>", function()
       vim.cmd.write({ bang = true })
       vim.cmd.bdelete()
+      require("toggleterm.terminal").Terminal
+        :new({
+          direction = "horizontal",
+          count = 2,
+          hidden = true,
+        })
+        :toggle()
     end, { buffer = true })
   end,
 })
