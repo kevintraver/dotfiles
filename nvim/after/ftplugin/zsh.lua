c3f7723d9b6636a3bdece3ce65c55c5900789ffd
Kevin Traver <kevintraver@kevintraver.com>
2024-10-13 04:16:52 -0700

    add <A-e> keymap for close when in zsh edit line

--- nvim/after/ftplugin/zsh.lua
+++ nvim/after/ftplugin/zsh.lua
@@ -12,6 +12,17 @@ vim.api.nvim_create_autocmd("BufEnter", {
         })
         :toggle()
     end, { buffer = true })
+    vim.keymap.set({ "n", "i" }, "<A-e>", function()
+      vim.cmd.write({ bang = true })
+      vim.cmd.bdelete()
+      require("toggleterm.terminal").Terminal
+        :new({
+          direction = "horizontal",
+          count = 2,
+          hidden = true,
+        })
+        :toggle()
+    end, { buffer = true })
     vim.keymap.set({ "n", "i" }, "<A-Enter>", function()
       vim.cmd.write({ bang = true })
       vim.cmd.bdelete()
