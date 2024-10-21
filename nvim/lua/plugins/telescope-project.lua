87ad0f33858c3034d53f29447c5b06f2ee87ae43
Kevin Traver <kevintraver@kevintraver.com>
2024-10-21 13:25:29 -0700

    telescope-project: add grep project files keymap

--- nvim/lua/plugins/telescope-project.lua
+++ nvim/lua/plugins/telescope-project.lua
@@ -2,6 +2,20 @@ return {
   "nvim-telescope/telescope-project.nvim",
   lazy = true,
   config = function()
+    require("telescope").setup({
+      extensions = {
+        project = {
+          mappings = {
+            n = {
+              ["g"] = require("telescope._extensions.project.actions").search_in_project_files,
+            },
+            i = {
+              ["<c-g>"] = require("telescope._extensions.project.actions").search_in_project_files,
+            },
+          },
+        },
+      },
+    })
     require("telescope").load_extension("project")
   end,
   keys = {
