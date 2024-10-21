28a7404a9f619b5662159544d4c0351ebbaf344d
Kevin Traver <kevintraver@kevintraver.com>
2024-10-20 23:21:18 -0700

    refjump: update keymap descriptions

--- nvim/lua/plugins/refjump.lua
+++ nvim/lua/plugins/refjump.lua
@@ -2,8 +2,14 @@ return {
   "mawkler/refjump.nvim",
   lazy = true,
   keys = {
-    "]r",
-    "[r",
+    {
+      "]r",
+      desc = "Jump to next LSP reference",
+    },
+    {
+      "[r",
+      desc = "Jump to previous LSP reference",
+    },
   },
   config = true,
 }
