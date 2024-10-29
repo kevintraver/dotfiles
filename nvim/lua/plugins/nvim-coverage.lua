8980a806a02abdd7aa30790448e1b6f768ef2afe
Kevin Traver <kevintraver@kevintraver.com>
2024-10-29 06:38:08 -0700

    fix nvim-coverage opts

--- nvim/lua/plugins/nvim-coverage.lua
+++ nvim/lua/plugins/nvim-coverage.lua
@@ -11,7 +11,7 @@ return {
     "CoverageClear",
     "CoverageSummary",
   },
-  config = {
+  opts = {
     signs = {
       covered = { priority = 80 }, -- use a higher value than diagnostics or gitsigns
       uncovered = { priority = 80 },
