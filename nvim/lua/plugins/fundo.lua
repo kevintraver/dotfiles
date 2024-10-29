c6a31c982a8ee43e1010ace7c04f5acdb541869f
Kevin Traver <kevintraver@kevintraver.com>
2024-10-29 11:52:48 -0700

    promise-async load verylazy

--- nvim/lua/plugins/fundo.lua
+++ nvim/lua/plugins/fundo.lua
@@ -1,7 +1,12 @@
 return {
   "kevinhwang91/nvim-fundo",
   event = "VeryLazy",
-  dependencies = { "kevinhwang91/promise-async" },
+  dependencies = {
+    {
+      "kevinhwang91/promise-async",
+      event = "VeryLazy",
+    },
+  },
   build = function()
     require("fundo").install()
   end,
