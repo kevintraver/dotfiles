fed2a88972bd93a5dc2bbb0844e03f1451ed92ed
Kevin Traver <kevintraver@kevintraver.com>
2024-11-01 16:13:50 -0700

    combine nvim-autopairs config

--- nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/lua/plugins/nvim-autopairs.lua
@@ -1,5 +1,6 @@
 return {
   "windwp/nvim-autopairs",
+  event = "InsertEnter",
   config = function()
     require("nvim-autopairs").setup({
       disable_filetype = {
@@ -8,5 +9,9 @@ return {
         "rip-substitute",
       },
     })
+    require("nvim-autopairs").setup()
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    local cmp = require("cmp")
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
   end,
 }
