c1afaad1163ab0bf926a39744ccf36db75f51087
Kevin Traver <kevintraver@kevintraver.com>
2024-11-02 15:26:47 -0700

    use blink instead of nvim-cmp

--- nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/lua/plugins/nvim-autopairs.lua
@@ -9,8 +9,5 @@ return {
         "rip-substitute",
       },
     })
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-    local cmp = require("cmp")
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
   end,
 }
