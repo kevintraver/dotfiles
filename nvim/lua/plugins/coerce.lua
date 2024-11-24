82a068a6dfef51bec5fbc4ec1e04d0a23be4061f
Kevin Traver <kevintraver@kevintraver.com>
2024-11-24 13:46:26 -0800

    update coerce config keymaps config

--- nvim/lua/plugins/coerce.lua
+++ nvim/lua/plugins/coerce.lua
@@ -1,44 +1,11 @@
 return {
   "gregorias/coerce.nvim",
-  enabled = true,
   event = "VeryLazy",
-  opts = function()
-    local coroutine_m = require("coerce.coroutine")
-    local selector_m = require("coerce.selector")
-    local transformer_m = require("coerce.transformer")
-    return {
-      modes = {
-        {
-          vim_mode = "n",
-          keymap_prefix = "gu",
-          selector = selector_m.select_current_word,
-          transformer = function(selected_region, apply)
-            return coroutine_m.fire_and_forget(
-              transformer_m.transform_lsp_rename_with_local_failover,
-              selected_region,
-              apply
-            )
-          end,
-        },
-        {
-          vim_mode = "n",
-          keymap_prefix = "gU",
-          selector = selector_m.select_current_word,
-          transformer = transformer_m.transform_local,
-        },
-        {
-          vim_mode = "n",
-          keymap_prefix = "guo",
-          selector = selector_m.select_with_motion,
-          transformer = transformer_m.transform_local,
-        },
-        {
-          vim_mode = "v",
-          keymap_prefix = "gu",
-          selector = selector_m.select_current_visual_selection,
-          transformer = transformer_m.transform_local,
-        },
-      },
-    }
-  end,
+  opts = {
+    default_mode_keymap_prefixes = {
+      normal_mode = "gu",
+      motion_mode = "guo",
+      visual_mode = "gu",
+    },
+  },
 }
