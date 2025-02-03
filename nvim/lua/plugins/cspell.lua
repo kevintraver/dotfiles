471cc9a2b641d045a6e2f23badf7ba71d0c03f82
Kevin Traver <kevintraver@kevintraver.com>
2025-02-02 19:55:58 -0800

    update cspell config to use null-ls

--- nvim/lua/plugins/cspell.lua
+++ nvim/lua/plugins/cspell.lua
@@ -8,16 +8,17 @@ return {
       event = "VeryLazy",
     },
   },
-  config = function()
-    require("null-ls").setup({
-      sources = {
-        require("cspell").diagnostics.with({
-          diagnostics_postprocess = function(diagnostic)
-            diagnostic.severity = vim.diagnostic.severity["WARN"]
-          end,
-        }),
-        require("cspell").code_actions,
-      },
-    })
+  opts = function(_, opts)
+    local cspell = require("cspell")
+    opts.sources = opts.sources or {}
+    table.insert(
+      opts.sources,
+      cspell.diagnostics.with({
+        diagnostics_postprocess = function(diagnostic)
+          diagnostic.severity = vim.diagnostic.severity.WARN
+        end,
+      })
+    )
+    table.insert(opts.sources, cspell.code_actions)
   end,
 }
