b3b193e225874d9f5a340a51341bf07f2e4ab158
Kevin Traver <kevintraver@kevintraver.com>
2025-03-29 16:11:01 -0700

    feat(neovim): use tokyonight night style instead of storm

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -1,7 +1,7 @@
 return {
   "folke/tokyonight.nvim",
   opts = {
-    style = "storm",
+    style = "night",
     on_highlights = function(highlights, colors)
       highlights.BufferLineFill = {
         bg = colors.bg_dark,
