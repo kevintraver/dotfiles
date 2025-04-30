becbb6049d9a2d4b0b1a8b21dd8ad29c390e6808
Kevin Traver <kevintraver@kevintraver.com>
2025-04-30 17:43:44 -0600

    feat(raycast): add restart yabai script

---
+++ raycast/scripts/restart-yabai.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Restart yabai
+# @raycast.mode silent
+
+# @raycast.icon 🖥
+
+# Documentation:
+# @raycast.description Restart the yabai service
+# @raycast.author kevintraver
+# @raycast.authorURL https://raycast.com/kevintraver
+
+/opt/homebrew/bin/yabai --restart-service
